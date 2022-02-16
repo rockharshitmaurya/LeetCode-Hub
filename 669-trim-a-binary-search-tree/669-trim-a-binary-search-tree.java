af863a004ad3caf5f471d72a1d10ed4a4c8e4769
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-02-16 19:01:51 +0530

    Time: 1 ms (23.11%), Space: 44.9 MB (18.45%) - LeetHub

--- 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.java
+++ 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.java
@@ -14,59 +14,45 @@
  * }
  */
 class Solution {
-    public TreeNode trimBST(TreeNode root, int low, int high) {
-        if(root==null) return root;
-        Stack<TreeNode> st=new Stack<>();
-        st.push(root);
-        if(root.val<low || root.val>high){
-            root= helper(root,low,high);
-        }
-        while(!st.isEmpty()){
-            TreeNode node=st.pop();
-            if(node.left!=null){
-                if(node.left.val<low || node.left.val>high){
-                    node.left=helper(node.left,low,high);
-                }
-            }
-            // else{
-                if(node.left!=null) st.push(node.left);
-            // }
-            if(node.right!=null){
-                if(node.right.val<low || node.right.val>high){
-                node.right=helper(node.right,low,high);
-                }
-            }
-            // else{
-                if(node.right!=null) st.push(node.right);
-            // }
-            
-        }
-        return root;
+	public TreeNode trimBST(TreeNode root, int low, int high) {
+		//Open Notes
+        if (root == null) return root;
+		Stack<TreeNode> st = new Stack<>();
+		st.push(root);
+		if (root.val < low || root.val > high) {
+			root = helper(root, low, high);
+		}
+		while (!st.isEmpty()) {
+			TreeNode node = st.pop();
+			if (node.left != null) {
+				if (node.left.val < low || node.left.val > high) {
+					node.left = helper(node.left, low, high);
+				}
+			}
+			if (node.left != null) st.push(node.left);
+			if (node.right != null) {
+				if (node.right.val < low || node.right.val > high) {
+					node.right = helper(node.right, low, high);
+				}
+			}
+			if (node.right != null) st.push(node.right);
 
-    }
-    
-    TreeNode helper(TreeNode root,int low,int high){
-            while(root!=null){
-                if(root.val>high){
-                    root=root.left;
-                }else if(root.val<low){
-                    root=root.right;
-                }else{
-                    return root;
-                }
-                
-            } 
-        return root;
-        // if(root.val< low){
-          //     while(root!=null && root.val<low){
-          //         root=root.right;
-          //     }
-          //     return root;
-          // }else{
-          //   while(root!=null && root.val>high){
-          //         root=root.left;
-          //     }
-          //     return root;  
-          // } 
-    }
+		}
+		return root;
+
+	}
+
+	TreeNode helper(TreeNode root, int low, int high) {
+		while (root != null) {
+			if (root.val > high) {
+				root = root.left;
+			} else if (root.val < low) {
+				root = root.right;
+			} else {
+				return root;
+			}
+
+		}
+		return root;
+	}
 }
