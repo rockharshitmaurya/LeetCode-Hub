4008f03d2a1953138192d0bad32ac8e4ae8d8670
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-02-19 23:50:02 +0530

    Time: 22 ms (77.78%), Space: 150.2 MB (11.11%) - LeetHub

--- 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.java
+++ 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.java
@@ -11,11 +11,4 @@ class Solution {
         list.add(sum+list.pollLast());
         return list;
     }
-//     int helper(long sum,long num,ArrayList<Long> list){
-//         if(num>sum) return num;
-//          int l=helper(sum,num+2,list);
-//        int r= helper(sum,num+4,list)
-        
-//         helper(sum,num+2);
-//     }
 }
