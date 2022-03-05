cefad64ee52c99aa06c3df29c962178678ce63dd
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-03-06 01:21:43 +0530

    Time: 318 ms (20.00%), Space: 122.1 MB (20.00%) - LeetHub

--- 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.java
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.java
@@ -1,26 +1,20 @@
 class Solution {
     public int[] sortJumbled(int[] mapping, int[] nums) {
-            // HashMap<Integer,Integer> map=new HashMap<>();
-            // for(int i=0; i<mapping.length; i++){
-            //     map.put(i,mapping[i]);
-            // }
-        int arr[][]=new int[nums.length][2];
-        int id=0;
-            for(int val:nums){
-                String str=val+"";
-                StringBuilder sb=new StringBuilder();
-                for(Character c:str.toCharArray()){
-                    int new_val=mapping[c-'0'];//map.get(c-'0');
-                    sb.append(new_val);
-                }
-                arr[id][0]=val;
-                arr[id++][1]=Integer.parseInt(sb.toString());
+        List<int[]> al = new ArrayList<>();
+        for(int n: nums){
+            String s = String.valueOf(n);
+            StringBuilder sb = new StringBuilder();
+            for(char c: s.toCharArray()){
+                sb.append(mapping[c-'0']);
+            }
+            al.add(new int[]{Integer.valueOf(sb.toString()), n});
         }
-        Arrays.sort(arr,(x,y)->x[1]-y[1]);
-        for(int i=0; i<nums.length; i++){
-            nums[i]=arr[i][0];
-        
+        Collections.sort(al, (a,b)-> a[0] - b[0]);
+        int[] ans = new int[nums.length];
+        int idx = 0;
+        for(int[] arr : al){
+            ans[idx++] = arr[1];
         }
-        return nums;
+        return ans;
     }
 }
