41770cb279c785a7b1d6b6f245108b47bb9500f7
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-03-28 01:05:10 +0530

    Time: 6 ms (50.00%), Space: 98.8 MB (50.00%) - LeetHub

--- 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.java
+++ 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.java
@@ -1,22 +1,9 @@
 class Solution {
     public int minDeletion(int[] nums) {
-        int ans=0;
-        ArrayList<Integer> list=new ArrayList<>();
-        for(int num:nums) list.add(num);
-        for(int i=0; i<list.size()-1; i++){
-            int a=list.get(i);
-            int b=list.get(i+1);
-            if(i%2==0 && a==b){
-                        // System.out.println(list.get(i)+" "+list.get(i+1));    
-                list.remove(i);
-                i--;
-                ans++;
-            }
+       int ans=0;
+        for(int i=0; i<nums.length-1; i++){
+            if((i-ans)%2==0 && nums[i]==nums[i+1]) ans++;
         }
-        if(list.size()%2!=0){
-            ans++;
-        }
-        // System.out.println(list);
-        return ans;
+        return ans+(nums.length-ans)%2;
     }
 }
