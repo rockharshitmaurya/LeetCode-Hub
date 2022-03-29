6d92aca6380a0e3c19be54ca93b8871cff523d7b
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-03-30 02:35:09 +0530

    Time: 19 ms (78.48%), Space: 65.8 MB (22.38%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.java
+++ 881-boats-to-save-people/881-boats-to-save-people.java
@@ -3,16 +3,9 @@ class Solution {
        Arrays.sort(people);
         int i=0,j=people.length-1,count=0;
         while(i<=j){
-            int sum=people[i];
-            if(sum+people[j]<=limit){
-                j--;
-                i++;
-                count++;
-            }else{
-                j--;
-                count++;
-            } 
-            // else if(i<people.length-1 && sum+people[i+1]<=limit) i++;
+            if(people[i]+people[j]<=limit) i++;
+            count++;
+            j--;
         }
         return count;
     }
