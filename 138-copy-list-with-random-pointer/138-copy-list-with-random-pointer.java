f0209eb1d787ce99de3b561773d8acec23d5b24e
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-03-31 11:24:12 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (98.65%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.java
@@ -15,34 +15,30 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
-       Node new_head=new Node(0);
-       Node ans=new_head;
-       Node temp=head;
-        HashMap<Node,Integer> map=new HashMap<>();
-        HashMap<Integer,Node> new_map=new HashMap<>();
-        int i=0;
-        while(temp!=null){
-            new_head.next=new Node(temp.val);
-            new_map.put(i,new_head.next);
-            map.put(temp,i++);
-            new_head=new_head.next;
-            temp=temp.next;
+        Node cur=head,front=head;
+        while(cur!=null){
+            front=cur.next;
+            Node dummy=new Node(cur.val);
+            cur.next=dummy;
+            dummy.next=front;
+            cur=front;
         }
-        temp=head;
-        new_head=ans.next;
-        HashMap<Integer,Integer> r_map=new HashMap<>();
-        i=0;
-        while(temp!=null){
-            r_map.put(i++,map.get(temp.random));
-            temp=temp.next;
+        cur=head;front=head;
+        while(cur!=null){
+            if(cur.random!=null){
+                cur.next.random=cur.random.next;
+            }
+            cur=cur.next.next;
         }
-        temp=head;
-        new_head=ans.next;
-        // System.out.println(r_map);
-        i=0;
-        while(new_head!=null){
-            new_head.random=new_map.get(r_map.get(i++));
-            new_head=new_head.next;
+        cur=head;front=head;
+        Node copy=new Node(0);
+        Node ans=copy;
+        while(cur!=null){
+            front=cur.next.next;
+            copy.next=cur.next;
+            cur.next=front;
+            cur=cur.next;
+            copy=copy.next;
         }
         return ans.next;
     }
