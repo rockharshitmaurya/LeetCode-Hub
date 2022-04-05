ebfea21188ddfa1dfa8c47e6319c4d91cfa057fa
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-04-06 01:24:27 +0530

    Time: 3 ms (84.97%), Space: 55 MB (64.36%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
@@ -2,22 +2,21 @@ class Solution {
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
         List<List<Integer>> ans=new ArrayList<>();
         List<Integer> help=new ArrayList<>();
-        Queue<List<Integer>> q=new LinkedList<>();
         help.add(0);
-        q.offer(help);
-        while(!q.isEmpty()){
-           help=q.poll();
-            int v=help.get(help.size()-1);
-            if(v==graph.length-1){
-                ans.add(help);
-                continue;
-            }
-            for(int i:graph[v]){
-                ArrayList<Integer> temp=new ArrayList<>(help);
-                temp.add(i);
-                q.offer(temp);
-            }
-        }
+        dfs(graph,ans,help,0);
+        
         return ans;
+    }
+    void dfs(int[][] graph,List<List<Integer>>  ans,List<Integer> help,int start){
+        if(start==graph.length-1){
+            ans.add(new ArrayList<>(help));
+            return;
+        }
+        for(int i:graph[start]){
+            help.add(i);
+            dfs(graph,ans,help,i);
+            help.remove(help.size()-1);
+        }
+
     }
 }
