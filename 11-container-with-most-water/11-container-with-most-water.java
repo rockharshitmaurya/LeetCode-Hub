03de483d638906c5810ae807323120031553a907
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-04-05 13:05:50 +0530

    Time: 5 ms (44.04%), Space: 80.8 MB (45.50%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.java
+++ 11-container-with-most-water/11-container-with-most-water.java
@@ -3,10 +3,9 @@ class Solution {
       int start=0,end=height.length-1;
         int ans=0;
         while(start<end){
-            int mul=end-start;
-            int min=Math.min(height[start],height[end]);
-            ans=Math.max(ans,(mul*min));
-            // System.out.print(ans+" "+mul+"-->"+min+" ");
+            // int mul=end-start;
+            // int min=Math.min(height[start],height[end]);
+            ans=Math.max(ans,((end-start)*Math.min(height[start],height[end])));
             if(height[start]<height[end]) start++;
             else end --;
         }
