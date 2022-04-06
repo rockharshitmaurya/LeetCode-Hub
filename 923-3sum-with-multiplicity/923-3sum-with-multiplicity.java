68342454b74d94eefe7bd027f417338b23f2a601
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-04-06 19:54:06 +0530

    Time: 1705 ms (5.24%), Space: 149.4 MB (6.05%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.java
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.java
@@ -1,14 +1,5 @@
 class Solution {
     public int threeSumMulti(int[] arr, int target) {
-       //  Set<Integer> set= new HashSet<>();
-       // // for(int i:arr) set.add(i);
-       //  int count=0;
-       //  for(int i=1; i<arr.length; i++){
-       //      int temp=target-arr[i]-arr[i-1];
-       //      if(set.contains(temp)) count++;
-       //      set.add(arr[i]);
-       //      set.add(arr[i-1]);
-       //  }
         int count=0;
         int mod = 1000000007;
         HashMap<Integer,Integer> map=new HashMap<>();
