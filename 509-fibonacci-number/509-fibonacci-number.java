1129dd57a307ae34c297c800acbc1e160b088aa0
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-04-16 13:49:33 +0530

    Time: 0 ms (100.00%), Space: 38.8 MB (93.16%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int fib(int n) {
+        if(n==0) return 0;
+        int arr[]=new int[n+1];
+        arr[1]=1;
+        for(int i=2; i<=n; i++){
+            arr[i]=arr[i-1]+arr[i-2];
+        }
+        return arr[n];
+    }
+}
