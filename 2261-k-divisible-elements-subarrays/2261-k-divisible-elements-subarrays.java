5a28d576cfd97819dd859a5efd5ffdd065550510
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-05-02 15:34:36 +0530

    Time: 231 ms (40.00%), Space: 87.1 MB (20.00%) - LeetHub

--- 2261-k-divisible-elements-subarrays/2261-k-divisible-elements-subarrays.java
+++ 2261-k-divisible-elements-subarrays/2261-k-divisible-elements-subarrays.java
@@ -2,13 +2,14 @@ class Solution {
     public int countDistinct(int[] nums, int k, int p) {
         Set<String> set=new HashSet<String>();
         for (int startPoint = 0; startPoint <nums.length ; startPoint++) {
-          for (int grps = startPoint; grps <=nums.length ; grps++) {
-             StringBuilder sb=new StringBuilder();
-             int div=0;
-               for (int j = startPoint ; j < grps ; j++) {
-                  sb.append((char)nums[j]);
-                  if(nums[j]%p==0) div++;
-               }
+            StringBuilder sb=new StringBuilder();
+            int div=0;
+          for (int grps = startPoint; grps <nums.length ; grps++) {
+             
+               // for (int j = startPoint ; j < grps ; j++) {
+                  sb.append((char)nums[grps]);
+                  if(nums[grps]%p==0) div++;
+               // }
                  if(div<=k && sb.length()!=0) set.add(sb.toString());
             
          }
