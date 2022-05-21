e3d167e715a5f9bf4ca6658a6d8dba48ef2838a0
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-05-22 03:05:31 +0530

    Time: 3 ms (72.57%), Space: 42.6 MB (76.50%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.java
+++ 695-max-area-of-island/695-max-area-of-island.java
@@ -1,5 +1,4 @@
 class Solution {
-    int count=0;
     public int maxAreaOfIsland(int[][] grid) {
        int n=grid.length,m=grid[0].length;
        boolean visited[][]=new boolean[n][m];
@@ -7,22 +6,23 @@ class Solution {
        for(int i=0; i<n; i++){
            for(int j=0; j<m; j++){
                if(grid[i][j]==1 && visited[i][j]==false){
-                   dfs(grid,i,j,n,m,visited);
+                   int count=dfs(grid,i,j,n,m,visited);
                    ans=Math.max(ans,count);
-                   count=0;
                }
            }
        }
         return ans;
     }
-    void dfs(int[][] grid,int i,int j,int n,int m,boolean visited[][]){
+    int dfs(int[][] grid,int i,int j,int n,int m,boolean visited[][]){
+        int ans=0;
         if(!(i<0 || i>=n || j<0 || j>=m || visited[i][j]==true || grid[i][j]==0)){
            visited[i][j]=true;
-           count++;
-           dfs(grid,i,j+1,n,m,visited); 
-           dfs(grid,i,j-1,n,m,visited); 
-           dfs(grid,i+1,j,n,m,visited); 
-           dfs(grid,i-1,j,n,m,visited); 
+           ans++;
+           ans+=dfs(grid,i,j+1,n,m,visited); 
+           ans+=dfs(grid,i,j-1,n,m,visited); 
+           ans+=dfs(grid,i+1,j,n,m,visited); 
+           ans+=dfs(grid,i-1,j,n,m,visited); 
         }
+        return ans;
     }
 }
