b093b2589d88dbd83587867bd1946191a44995c1
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-06-22 18:11:55 +0530

    Time: 5 ms (69.12%), Space: 44.4 MB (61.40%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -1,5 +1,6 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
+        shuffle(nums);
         return quickSelect(nums,0,nums.length-1,nums.length-k);
     }
     int quickSelect(int nums[],int start,int end,int k){
@@ -14,6 +15,13 @@ class Solution {
         if(left==k) return nums[k];
         else if(left<k) return quickSelect(nums,left+1,end,k);
         else return quickSelect(nums,start,left-1,k);
+    }
+    void shuffle(int nums[]){
+        Random rnd=new Random();
+        for(int i=0; i<nums.length; i++){
+            int idx=rnd.nextInt(i+1);
+            swap(nums,i,idx);
+        }
     }
     void swap(int nums[],int i,int j){
         int temp=nums[i];
