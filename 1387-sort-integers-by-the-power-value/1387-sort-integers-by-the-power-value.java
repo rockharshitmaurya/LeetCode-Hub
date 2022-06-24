5996c735a4f351310a831968d2ace0449b04cc7b
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-06-25 00:26:37 +0530

    Time: 67 ms (83.29%), Space: 42.3 MB (86.63%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.java
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.java
@@ -1,14 +1,19 @@
 class Solution {
     public int getKth(int lo, int hi, int k) {
         int arr[][]=new int[hi-lo+1][2];
+        HashMap<Integer, Integer> map=new HashMap<>();
         int idx=0;
         for(int i=lo; i<=hi; i++){
             int num=i,pow=0;
-            while(num!=1){
-                if(num%2==0) num/=2;
-                else num=3*num+1;
-                
-                pow++;
+            if(!map.containsKey(i)){
+                while(num!=1){
+                    if(num%2==0) num/=2;
+                    else num=3*num+1;
+                    pow++;
+                }
+                map.put(i,pow);
+            }else{
+                pow=map.get(i);
             }
             arr[idx][0]=i; arr[idx][1]=pow; idx++;
         }
