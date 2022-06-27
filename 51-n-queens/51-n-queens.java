524ecf9a7fe71f77ebf38c57222c1a3c7f2896a5
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-06-27 15:27:07 +0530

    Time: 5 ms (67.02%), Space: 44.7 MB (78.73%) - LeetHub

--- 51-n-queens/51-n-queens.java
+++ 51-n-queens/51-n-queens.java
@@ -1,61 +1,57 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
-        List<List<String>> ans=new ArrayList<List<String>>();
         char board[][]=new char[n][n];
+        List<List<String>> ans=new ArrayList<>();
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
                 board[i][j]='.';
             }
         }
-        helper(board,ans,0);
+        helper(0,board,ans);
         return ans;
     }
-    void helper(char board[][],List<List<String>> list,int row){
+void helper(int row,char board[][],List<List<String>> ans){
         if(row==board.length){
-            list.add(answerMake(board));
+            ArrayList<String> res=new ArrayList<>();
+            for(char ch[]:board){
+                res.add(new String(ch));
+            }
+            ans.add(res);
             return;
         }
         for(int col=0; col<board.length; col++){
-            if(board[row][col]=='.' && isQueenSafe(board,row,col)==true){
+            if(board[row][col]=='.' && isQueenSafe(board,row,col)){
                 board[row][col]='Q';
-                helper(board,list,row+1);
+                helper(row+1,board,ans);
                 board[row][col]='.';
             }
         }
-        
     }
     boolean isQueenSafe(char chess[][],int row,int col){
-        for(int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--){
-            if(chess[i][j] == 'Q'){
-                return false;
+            for(int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--){
+                if(chess[i][j] == 'Q'){
+                    return false;
+                }
             }
-        }
 
-        for(int i = row - 1, j = col; i >= 0; i--){
-            if(chess[i][j] == 'Q'){
-                return false;
+            for(int i = row - 1, j = col; i >= 0; i--){
+                if(chess[i][j] == 'Q'){
+                    return false;
+                }
             }
-        }
 
-        for(int i = row - 1, j = col + 1; i >= 0 && j < chess.length; i--, j++){
-            if(chess[i][j] == 'Q'){
-                return false;
+            for(int i = row - 1, j = col + 1; i >= 0 && j < chess.length; i--, j++){
+                if(chess[i][j] == 'Q'){
+                    return false;
+                }
             }
-        }
 
-        for(int i = row, j = col - 1; j >= 0; j--){
-            if(chess[i][j] == 'Q'){
-                return false;
+            for(int i = row, j = col - 1; j >= 0; j--){
+                if(chess[i][j] == 'Q'){
+                    return false;
+                }
             }
-        }
 
-        return true;
-    }
-    List<String> answerMake(char ch[][]){
-        List<String> ans=new ArrayList<>();
-        for(int i=0; i<ch.length; i++){
-            ans.add(new String(ch[i]));
-        }
-        return ans;
+            return true;
     }
 }
