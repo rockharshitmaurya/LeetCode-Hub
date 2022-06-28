7a45c20cde8cafed1323bdd90183c51a4657adbc
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-06-28 16:55:34 +0530

    Time: 15 ms (69.91%), Space: 53.9 MB (76.54%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -2,24 +2,18 @@ class Solution {
     public int minDeletions(String s) {
         int count[]=new int[26];
         for(char ch:s.toCharArray()) count[ch-'a']++;
-        Arrays.sort(count);
+        HashSet<Integer> set=new HashSet<>();
         int ans=0;
-        for(int i=24; i>=0; i--){
-            if(count[i]==0) break;
-            // if(count[i]==count[i+1]){
-                while(isExist(count,count[i],i)){
-                    count[i]--;
+        for(int i=0; i<26; i++){
+                while(count[i]>0){
+                    if(!set.contains(count[i])){
+                        set.add(count[i]);
+                        break;
+                    }
                     ans++;
-                    if(count[i]==0)  break;
+                    count[i]--;
                 }
-            // }
         }
         return ans;
-    }
-    boolean isExist(int count[],int val,int idx){
-        for(int i=idx+1; i<26; i++){
-            if(count[i]==val) return true;
-        }
-        return false;
     }
 }
