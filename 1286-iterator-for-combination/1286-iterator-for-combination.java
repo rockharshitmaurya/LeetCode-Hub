cb3b1719e41b5a4c7862ea46a01508a814a1d2ed
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-06-29 18:01:00 +0530

    Time: 57 ms (5.08%), Space: 51.4 MB (5.08%) - LeetHub

---
+++ 1286-iterator-for-combination/1286-iterator-for-combination.java
@@ -0,0 +1,30 @@
+class CombinationIterator {
+    PriorityQueue<String> pq=new PriorityQueue<>();
+    public CombinationIterator(String ch, int cl) {
+        helper(ch,cl,0,"");
+    }
+    public void helper(String ch,int cl,int idx, String str){
+        if(idx==ch.length()){
+            if(str.length()==cl) pq.add(str);
+            return;
+        }
+        str+=ch.charAt(idx)+"";
+        helper(ch,cl,idx+1,str);
+        str=str.substring(0,str.length()-1);
+        helper(ch,cl,idx+1,str);
+    }
+    public String next() {
+       return pq.poll(); 
+    }
+    
+    public boolean hasNext() {
+        return !pq.isEmpty();
+    }
+}
+
+/**
+ * Your CombinationIterator object will be instantiated and called as such:
+ * CombinationIterator obj = new CombinationIterator(characters, combinationLength);
+ * String param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
