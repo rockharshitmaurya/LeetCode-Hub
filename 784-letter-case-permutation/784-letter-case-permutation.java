06d77e50930830640a0af3942e7710ca48ed2579
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-06-29 23:14:38 +0530

    Time: 6 ms (52.26%), Space: 53.6 MB (74.45%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.java
+++ 784-letter-case-permutation/784-letter-case-permutation.java
@@ -3,12 +3,12 @@ class Solution {
     public List<String> letterCasePermutation(String s) {
         char ch[]=s.toCharArray();
         List<String> ans=new ArrayList<String>();
-        char a='a',A='A';
-        for(int i=0; i<26; i++){
-            map.put(a,A);
-            map.put(A,a);
-            a++;A++;
-        }
+        // char a='a',A='A';
+        // for(int i=0; i<26; i++){
+        //     map.put(a,A);
+        //     map.put(A,a);
+        //     a++;A++;
+        // }
         helper(ch,0,ans);
         return ans;
     }
@@ -16,9 +16,9 @@ class Solution {
             ans.add(new String(ch));
         for(int i=idx; i<ch.length; i++){
             if(Character.isDigit(ch[i])) continue;
-            ch[i]=map.get(ch[i]);
+            ch[i]=Character.isUpperCase(ch[i])?Character.toLowerCase(ch[i]):Character.toUpperCase(ch[i]);   
             helper(ch,i+1,ans);
-            ch[i]=map.get(ch[i]);
+            ch[i]=Character.isUpperCase(ch[i])?Character.toLowerCase(ch[i]):Character.toUpperCase(ch[i]);
         }
     }
 }
