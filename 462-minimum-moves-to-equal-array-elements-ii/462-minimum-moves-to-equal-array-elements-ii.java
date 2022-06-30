cfc992b706a5e31d238fb89cbf3452a9fc75cb8e
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-06-30 11:42:01 +0530

    Time: 5 ms (66.60%), Space: 45.9 MB (36.27%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
@@ -3,8 +3,9 @@ class Solution {
         Arrays.sort(nums);
         int idx=nums.length/2;
         int ans=0;
-        for(int i=0; i<nums.length; i++){
+        for(int i=0; i*2<nums.length; i++){
             ans+=Math.abs(nums[idx]-nums[i]);
+            ans+=Math.abs(nums[idx]-nums[nums.length-i-1]);
         }
       return ans;  
     }
