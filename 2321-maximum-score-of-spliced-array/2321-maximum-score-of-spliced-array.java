3a853894ec31fbc9c67c746cd1f7539d88122b63
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-06-30 20:23:38 +0530

    Time: 29 ms (5.23%), Space: 90.5 MB (21.81%) - LeetHub

---
+++ 2321-maximum-score-of-spliced-array/2321-maximum-score-of-spliced-array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maximumsSplicedArray(int[] nums1, int[] nums2) {
+        int sum1=0,sum2=0,res1=0,res2=0,max1=0,max2=0,ans=0;
+        for(int i=0; i<nums1.length; i++){
+            sum1+=nums1[i]; sum2+=nums2[i];
+            
+            res1+=(nums2[i]-nums1[i]);
+            res2+=(nums1[i]-nums2[i]);
+            
+            max1=Math.max(max1,res1);
+            max2=Math.max(max2,res2);
+            
+            if(res1<0) res1=0;
+            if(res2<0) res2=0;
+
+        }
+        System.out.println(sum1+" "+sum2+" "+max1+" "+max2);
+        ans=Math.max(sum1,sum2);
+        ans=Math.max(ans,sum1+max1);
+        ans=Math.max(ans,sum2+max2);
+        return ans;
+    }
+}
