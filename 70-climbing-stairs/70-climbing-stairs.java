b013a8e0daae9fc6fcb3c458b9a9fa9678486241
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-06-30 23:17:37 +0530

    Time: 3 ms (11.06%), Space: 40.6 MB (70.20%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,13 +1,11 @@
 class Solution {
-    int dp[]=new int[46];
-    // Arrays.fill(dp,-1);
     public int climbStairs(int n) {
-       if(n==0) return 1;
-        if(n<0) return 0;
-        if(dp[n]!=0) return dp[n];
-        
-        int val=climbStairs(n-1)+ climbStairs(n-2);
-        dp[n]=val;
-        return val;
+        int dp[]=new int[n+1];
+        dp[0]=1; dp[1]=1;
+        for(int i=2; i<=n; i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        System.out.println(Arrays.toString(dp));
+        return dp[n];
     }
 }
