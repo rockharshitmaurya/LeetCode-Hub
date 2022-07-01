8cce0d40f233b73ce4225aa58e196fecffcb9b24
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-07-01 22:53:34 +0530

    Time: 1 ms (36.23%), Space: 41.8 MB (26.79%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.java
+++ 213-house-robber-ii/213-house-robber-ii.java
@@ -4,21 +4,24 @@ class Solution {
         if(n==1) return nums[0];
         int first[]=new int[n-1];
         int last[]=new int[n-1];
+        //since we know that the house are arrenged in circulr order there by we know that we can not have first and the last element of the array togather ,so-->
         for(int i=0; i<n; i++){
-            if(i!=0) first[i-1]=nums[i];
-            if(i!=n-1) last[i]=nums[i];
+            if(i!=0) first[i-1]=nums[i]; //once we ignore the first element and copy rest of the element in onther array
+            if(i!=n-1) last[i]=nums[i]; // and make one another array in which we add all the element except tha last elemnt of ate array 
         }
-        return Math.max(helper(first,n-1),helper(last,n-1));
+        return Math.max(helper(first,n-1),helper(last,n-1)); //now we apply same logic as house robber 1 problm and return the max of them
         
     }
     int helper(int nums[],int n){
-        int dp[]=new int[n];
-        dp[0]=nums[0];
+        int prev=nums[0];
+        int prev2=0;
         for(int i=1; i<n; i++){
-            int take=nums[i]+((i>1)?dp[i-2]:0);
-            int notTake=0+dp[i-1];
-            dp[i]=Math.max(take,notTake);
+            int take=nums[i]+((i>1)?prev2:0);
+            int notTake=0+prev;
+            int cur=Math.max(take,notTake);
+            prev2=prev;
+            prev=cur;
         }
-        return dp[n-1];
+        return prev;
     }
 }
