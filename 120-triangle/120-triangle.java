35a3d4c7b8c073fd9315d09c8610d32dc5d45c7e
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-07-02 23:53:38 +0530

    Time: 7 ms (35.23%), Space: 43.7 MB (67.66%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -1,17 +1,15 @@
 class Solution {
-    public int minimumTotal(List<List<Integer>> triangle) {
-        List<List<Integer>> memo=new ArrayList<>();
-        for(int i=0; i<triangle.size(); i++){
-            memo.add(new ArrayList<Integer>(Collections.nCopies(triangle.get(i).size(), -1)));
+    public int minimumTotal(List<List<Integer>> tngl) {
+        for(int i=tngl.size()-2; i>=0; i--){
+            for(int j=0; j<tngl.get(i).size(); j++){
+                int down=tngl.get(i+1).get(j);
+                // System.out.println(i+" "+j+" "+tngl.get(i));
+                int dig=tngl.get(i+1).get(j+1);
+                int cur=tngl.get(i).get(j);
+                tngl.get(i).set(j,Math.min(down,dig)+cur);
+                
+            }
         }
-        return helper(triangle,0,0,memo);
-    }
-    int helper(List<List<Integer>> triangle,int level,int idx,List<List<Integer>> memo){
-        if(level==triangle.size()-1) return triangle.get(level).get(idx);
-        if(memo.get(level).get(idx)!=-1) return memo.get(level).get(idx);
-        int dwn=triangle.get(level).get(idx)+helper(triangle,level+1,idx,memo);
-        int dig=triangle.get(level).get(idx)+helper(triangle,level+1,idx+1,memo);
-        memo.get(level).add(idx,Math.min(dwn,dig));
-        return Math.min(dwn,dig);
+        return tngl.get(0).get(0);
     }
 }
