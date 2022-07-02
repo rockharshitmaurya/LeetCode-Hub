6cc8a0227085a21c23af46fcf6fd5bb8a392b674
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-07-02 15:04:45 +0530

    Time: 1 ms (56.30%), Space: 42.2 MB (57.05%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -2,28 +2,22 @@ class Solution {
     public int uniquePathsWithObstacles(int[][] og) {
         int m=og.length,n=og[0].length;
         if(og[m-1][n-1]==1 || og[0][0]==1) return 0;
-        int dp[][]=new int[m][n]; dp[0][0]=1;
-        for(int i=1; i<n; i++) dp[0][i]=og[0][i]==1?0:dp[0][i-1];
-        for(int i=1; i<m; i++) dp[i][0]=og[i][0]==1?0:dp[i-1][0];
-        
-        for(int i=1; i<m; i++){
-            for(int j=1; j<n; j++){
-                dp[i][j]=og[i][j]==0?(dp[i-1][j]+dp[i][j-1]):0;
+        int dp[][]=new int[m][n]; 
+        // dp[0][0]=1;
+        // for(int i=1; i<n; i++) dp[0][i]=og[0][i]==1?0:dp[0][i-1];
+        // for(int i=1; i<m; i++) dp[i][0]=og[i][0]==1?0:dp[i-1][0];
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i==0 && j==0) dp[i][j]=1;
+                else if(og[i][j]==1) dp[i][j]=0;
+                else{
+                    int up=0,left=0;
+                    if(i>0) up=dp[i-1][j];
+                    if(j>0) left=dp[i][j-1];
+                    dp[i][j]=up+left;
+                }
             }
         }
-        for(int arr[]:dp)
-            System.out.println(Arrays.toString(arr));
         return dp[m-1][n-1];
-    }
-     int helper(int grid[][],int row,int col,int memo[][]){
-        if(row==0 && col==0){
-            return 1;
-        }
-        if(row<0 || col<0 || grid[row][col]==1) return 0;
-        if(memo[row][col]!=-1) return memo[row][col];
-        int l=helper(grid,row-1,col,memo);
-        int r=helper(grid,row,col-1,memo);
-        memo[row][col]=l+r;
-        return l+r;
     }
 }
