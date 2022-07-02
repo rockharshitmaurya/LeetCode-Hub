efe7d238448ffb17f12363c9deb1be8e1eb45739
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-07-02 11:07:43 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (56.57%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -5,9 +5,10 @@ class Solution {
         for(int i=1; i<m; i++){
             int temp[]=new int[n]; temp[0]=1;
             for(int j=1; j<n; j++){
-                temp[j]+=(dp[j]+temp[j-1]);
+                // temp[j]+=(dp[j]+temp[j-1]);
+                dp[j]+=dp[j-1];
             }
-            dp=temp;
+            // dp=temp;
         }
         return dp[n-1];
     }
