5017691ec37a83239c7235f3f7ebf4eb5a9ca16a
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-07-02 11:37:25 +0530

    Time: 1 ms (56.30%), Space: 42.2 MB (49.80%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -1,16 +1,25 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-       int width = obstacleGrid[0].length;
-    int[] dp = new int[width];
-    dp[0] = 1;
-    for (int[] row : obstacleGrid) {
-        for (int j = 0; j < width; j++) {
-            if (row[j] == 1)
-                dp[j] = 0;
-            else if (j > 0)
-                dp[j] += dp[j - 1];
-        }
+    public int uniquePathsWithObstacles(int[][] og) {
+        int m=og.length,n=og[0].length;
+        int memo[][]=new int[m][n];
+        for(int arr[]:memo)
+            Arrays.fill(arr,-1);
+        return helper(og,m-1,n-1,memo);
     }
-    return dp[width - 1]; 
+     int helper(int grid[][],int row,int col,int memo[][]){
+        if(row==0 && col==0){
+            if(grid[0][0]==1) return 0;
+            else return 1;
+        }
+        if(row<0 || col<0 || row>=grid.length || col>=grid[0].length || grid[row][col]==1) return 0;
+        if(memo[row][col]!=-1) return memo[row][col];
+        // grid[row][col]=-1;
+         // memo[row][col]=0;
+        int l=helper(grid,row-1,col,memo);
+        int r=helper(grid,row,col-1,memo);
+         // memo[row][col]=-1;
+        // grid[row][col]=0;
+        memo[row][col]=l+r;
+        return l+r;
     }
 }
