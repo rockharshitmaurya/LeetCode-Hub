60288f890c90c9c66d6cc364e17d21a15faf097d
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-07-02 11:37:58 +0530

    Time: 1 ms (56.30%), Space: 41.8 MB (76.97%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -11,14 +11,10 @@ class Solution {
             if(grid[0][0]==1) return 0;
             else return 1;
         }
-        if(row<0 || col<0 || row>=grid.length || col>=grid[0].length || grid[row][col]==1) return 0;
+        if(row<0 || col<0 || grid[row][col]==1) return 0;
         if(memo[row][col]!=-1) return memo[row][col];
-        // grid[row][col]=-1;
-         // memo[row][col]=0;
         int l=helper(grid,row-1,col,memo);
         int r=helper(grid,row,col-1,memo);
-         // memo[row][col]=-1;
-        // grid[row][col]=0;
         memo[row][col]=l+r;
         return l+r;
     }
