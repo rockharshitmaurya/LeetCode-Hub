228e380f926826a0ef0d951b64789ec080cfa251
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-07-04 21:06:09 +0530

    Time: 66 ms (14.88%), Space: 49 MB (24.51%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -1,14 +1,33 @@
 class Solution {
     public int cherryPickup(int[][] grid) {
-        
-        //recursion + memoization
-        
-        int row=grid.length,col=grid[0].length;
-        int dp[][][]=new int[row][col][col];
-        for(int arr[][]:dp)
-            for(int arr1[]:arr) 
-                Arrays.fill(arr1,-1);
-       return helper(grid,0,0,grid[0].length-1,grid[0].length,dp); 
+        int n=grid.length,m=grid[0].length;
+        int dp[][][]=new int[n][m][m];
+        for(int dj1=0; dj1<m; dj1++){
+            for(int dj2=0; dj2<m; dj2++){
+                if(dj1==dj2) dp[n-1][dj1][dj2]=grid[n-1][dj1];
+                else dp[n-1][dj1][dj2]=grid[n-1][dj1]+grid[n-1][dj2];
+            }
+        }
+        for(int i=n-2; i>=0; i--){
+            for(int col1=0; col1<m; col1++){
+                for(int col2=0; col2<m; col2++){
+                    int max=(int)-1e8;
+                    for(int dj1=-1; dj1<=1; dj1++){
+                        for(int dj2=-1; dj2<=1; dj2++){
+                        int val=0;
+                        if(col1==col2) val=grid[i][col1];
+                        else val=grid[i][col1]+grid[i][col2];
+                        if(col1+dj1>=0 && col1+dj1<m && col2+dj2>=0 && col2+dj2<m)
+                            val+=dp[i+1][col1+dj1][col2+dj2];
+                        else val+=(int)-1e8;
+                        max=Math.max(max,val);
+                        }
+                    }
+                    dp[i][col1][col2]=max;
+                }
+            }
+        }
+       return dp[0][0][m-1]; 
     }
     int helper(int grid[][],int row,int col1,int col2,int len,int dp[][][]){
         if(col1<0 || col2<0 || col1>=len || col2>=len) return (int)-1e8;
