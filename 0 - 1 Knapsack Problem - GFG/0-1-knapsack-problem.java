eca85985dd8a369c2fc2bb88f006c232a051f7e3
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-07-07 16:10:21 +0530

    Added solution - LeetHub

---
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
@@ -0,0 +1,72 @@
+// { Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class gfg
+{
+    public static void main(String args[])throws IOException
+    {
+        //reading input using BufferedReader class
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        
+        //reading total testcases
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            //reading number of elements and weight
+            int n = Integer.parseInt(read.readLine());
+            int w = Integer.parseInt(read.readLine());
+            
+            int val[] = new int[n];
+            int wt[] = new int[n];
+            
+            String st[] = read.readLine().trim().split("\\s+");
+            
+            //inserting the values
+            for(int i = 0; i < n; i++)
+              val[i] = Integer.parseInt(st[i]);
+             
+            String s[] = read.readLine().trim().split("\\s+"); 
+            
+            //inserting the weigths
+            for(int i = 0; i < n; i++)
+              wt[i] = Integer.parseInt(s[i]);
+              
+            //calling method knapSack() of class Knapsack
+            System.out.println(new Solution().knapSack(w, wt, val, n));
+        }
+    }
+}
+
+
+
+// } Driver Code Ends
+
+
+class Solution 
+{ 
+    //Function to return max value that can be put in knapsack of capacity W.
+    static int knapSack(int W, int wt[], int val[], int n) 
+    { 
+      int dp[][]=new int[n][W+1];
+      for(int arr[]:dp)
+        Arrays.fill(arr,-1);
+      return helper(n-1,W,wt,val,n,dp);   
+    } 
+    static int helper(int idx,int W,int wt[],int val[],int n,int dp[][]){
+        if(idx==0) {
+            if(wt[0]<=W) return val[0];
+            return 0;
+        }
+        if(dp[idx][W]!=-1) return dp[idx][W];
+        int take=0;
+        if(wt[idx]<=W) take=val[idx]+helper(idx-1,W-wt[idx],wt,val,n,dp);
+        int notTake=helper(idx-1,W,wt,val,n,dp);
+        return dp[idx][W]=Math.max(take,notTake);
+        // return dp[idx];
+    }
+}
+
+
