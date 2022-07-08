01c0b139dfc8f6359146f6ad5747b2ee84d43be3
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-07-08 13:33:21 +0530

    Time: 63 ms (5.62%), Space: 40.8 MB (59.69%) - LeetHub

--- 343-integer-break/343-integer-break.java
+++ 343-integer-break/343-integer-break.java
@@ -1,11 +1,12 @@
 class Solution {
     int ans=0;
     public int integerBreak(int n) {
-        if(n==2) return 1;
-        else if(n==3) return 2;
-        int arr[]=new int[n];
-        for(int i=0; i<n; i++) arr[i]=i+1;
-        helper(n-1,arr,n,1);
+        // if(n==2) return 1;
+        // else if(n==3) return 2;
+        
+        int arr[]=new int[n-1];
+        for(int i=0; i<n-1; i++) arr[i]=i+1;
+        helper(n-2,arr,n,1);
         return ans;
     }
     void helper(int idx,int arr[],int n,int pro){
