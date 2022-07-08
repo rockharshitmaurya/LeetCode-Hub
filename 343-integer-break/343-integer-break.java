ee6b7d7dbdac0dcec09d4711431cdd3d4943b02f
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-07-08 14:28:47 +0530

    Time: 3 ms (10.05%), Space: 41.2 MB (24.54%) - LeetHub

--- 343-integer-break/343-integer-break.java
+++ 343-integer-break/343-integer-break.java
@@ -5,13 +5,11 @@ class Solution {
         int arr[]=new int[n-1];
         int len=arr.length;
         int dp[][]=new int[len][n+1];
-        for(int i=0; i<len; i++) arr[i]=i+1;
-        
         for(int i=0; i<len; i++) dp[i][0]=1;
         for(int idx=1; idx<len; idx++){
             for(int k=1; k<=n; k++){
                 int left=1,right=1;
-                if(arr[idx]<=k) left=arr[idx]*dp[idx][k-arr[idx]];
+                if((idx+1)<=k) left=(idx+1)*dp[idx][k-(idx+1)];
                 right=dp[idx-1][k];
                 dp[idx][k]=Math.max(left,right);
             }
