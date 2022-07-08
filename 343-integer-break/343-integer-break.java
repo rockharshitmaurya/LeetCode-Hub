f52251eb2415260bf680c891c2fb6f38256945d5
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-07-08 14:02:52 +0530

    Time: 1 ms (69.07%), Space: 41.7 MB (5.77%) - LeetHub

--- 343-integer-break/343-integer-break.java
+++ 343-integer-break/343-integer-break.java
@@ -1,23 +1,25 @@
 class Solution {
     int ans=0;
-    public int integerBreak(int n) {
-        // if(n==2) return 1;
-        // else if(n==3) return 2;
-        
+    public int integerBreak(int n) {        
         int arr[]=new int[n-1];
+        int dp[][]=new int[n][n+1];
+        // Arrays.fill(dp,-1);
+        for(int sub[]:dp) Arrays.fill(sub,-1);
         for(int i=0; i<n-1; i++) arr[i]=i+1;
-        helper(n-2,arr,n,1);
+        ans=helper(n-2,arr,n,1,dp);
         return ans;
     }
-    void helper(int idx,int arr[],int n,int pro){
-        if(idx==-1){
+    int  helper(int idx,int arr[],int n,int pro,int dp[][]){
+        if(idx==-1) return 0;
             if(n==0){
-                ans=Math.max(ans,pro);
+                return 1;
             }
-            return;
-        }
-        
-        if(arr[idx]<=n) helper(idx,arr,n-arr[idx],pro*arr[idx]);
-        helper(idx-1,arr,n,pro);
+            // return 0;
+        // }
+        if(dp[idx][n]!=-1) return dp[idx][n];
+        int left=1,right=1;
+        if(arr[idx]<=n) left=arr[idx]*helper(idx,arr,n-arr[idx],pro*arr[idx],dp);
+        right=helper(idx-1,arr,n,pro,dp);
+        return dp[idx][n]=Math.max(left,right);
     }
 }
