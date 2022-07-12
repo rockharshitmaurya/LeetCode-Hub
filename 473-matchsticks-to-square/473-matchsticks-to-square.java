2e0519918ef6adaa6e34f9d7eb32295b28351b4c
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-07-12 11:46:47 +0530

    Time: 285 ms (21.11%), Space: 41.9 MB (56.74%) - LeetHub

---
+++ 473-matchsticks-to-square/473-matchsticks-to-square.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean makesquare(int[] arr) {
+        long sum=0;
+        for(int num:arr) sum+=num;
+        if(sum%4!=0) return false;
+        Arrays.sort(arr);
+        reverse(arr);
+        return helper(arr,((int)sum/4),0,new int[4]);
+    }
+    boolean helper(int arr[],int target,int idx,int sum[]){
+        if(idx==arr.length){
+            return true;
+        }
+            for(int i=0; i<4; i++){
+                if(sum[i]+arr[idx]>target) continue;
+                sum[i]+=arr[idx];
+                if(helper(arr,target,idx+1,sum)) return true;
+                sum[i]-=arr[idx];
+            }
+        return false;
+    }
+    private void reverse(int[] nums) {
+        int i = 0, j = nums.length - 1;
+        while (i < j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++; j--;
+        }
+    }
+}
