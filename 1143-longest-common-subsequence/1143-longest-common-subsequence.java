5ce1e9fc45db2afa823e7d766c85d0c2a3786f24
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-07-13 00:43:30 +0530

    Time: 17 ms (72.99%), Space: 46.1 MB (77.83%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -2,15 +2,15 @@ class Solution {
     int dp[][];
     public int longestCommonSubsequence(String text1, String text2) {
         int n=text1.length(),m=text2.length();
-        dp=new int[n][m];
+        dp=new int[n+1][m+1];
         for(int arr[]:dp) Arrays.fill(arr,-1);
-        return helper(text1,text2,n-1,m-1);
+        return helper(text1,text2,n,m);
         
     }
     int helper(String str1,String str2,int idx1,int idx2){
-        if(idx1<0 || idx2<0) return 0;
+        if(idx1==0 || idx2==0) return 0;
         if(dp[idx1][idx2]!=-1) return dp[idx1][idx2];
-        if(str1.charAt(idx1)==str2.charAt(idx2)) return 1+helper(str1,str2,idx1-1,idx2-1);
+        if(str1.charAt(idx1-1)==str2.charAt(idx2-1)) return 1+helper(str1,str2,idx1-1,idx2-1);
         int left=helper(str1,str2,idx1-1,idx2);
         int right=helper(str1,str2,idx1,idx2-1);
         return dp[idx1][idx2]=Math.max(left,right);
