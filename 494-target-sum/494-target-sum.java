f99f1e6809021a6a5515e37516f7f106643cd85d
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-07-12 16:41:49 +0530

    Time: 349 ms (39.12%), Space: 67.4 MB (13.12%) - LeetHub

--- 494-target-sum/494-target-sum.java
+++ 494-target-sum/494-target-sum.java
@@ -1,4 +1,5 @@
 class Solution {
+    HashMap<String,Integer> map=new HashMap<>();
     public int findTargetSumWays(int[] nums, int target) {
         int n=nums.length;
         // int dp[][]=new int[n][target+1];
@@ -9,9 +10,11 @@ class Solution {
             if(target==0) return 1;
             else return 0;
         }
-        
+        String key=idx+":"+target;
+        if(map.containsKey(key)) return map.get(key);
         int left=helper(nums,idx-1,target-nums[idx]);
         int mid=helper(nums,idx-1,target-(-nums[idx]));
+        map.put(key,(left+mid));
         return left+mid;
     }
 }
