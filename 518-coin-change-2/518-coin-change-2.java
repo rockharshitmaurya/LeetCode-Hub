e713b572b751292e3f1c6eb47b39c8a69408e717
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-07-12 18:38:28 +0530

    Time: 22 ms (19.53%), Space: 50.9 MB (40.24%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.java
+++ 518-coin-change-2/518-coin-change-2.java
@@ -2,8 +2,21 @@ class Solution {
     int dp[][];
     public int change(int amount, int[] coins) {
         dp = new int[coins.length][amount+1];
-        for(int arr[]:dp) Arrays.fill(arr,-1);
-        return helper(coins,amount,coins.length-1,"");
+        // for(int arr[]:dp) Arrays.fill(arr,-1);
+        for(int am=0; am<=amount; am++){
+            if(am%coins[0]==0) 
+                dp[0][am] = 1; 
+        }
+        for(int idx=1; idx<coins.length; idx++){
+            for(int am=0; am<=amount; am++){
+                int take=0;
+                if(coins[idx]<=am) take=dp[idx][am-coins[idx]];
+                int notTake=dp[idx-1][am];
+                dp[idx][am]=(take+notTake);
+            }
+        }
+        // return helper(coins,amount,coins.length-1,"");
+        return dp[coins.length-1][amount];
     }
     int helper(int coins[],int amount,int idx,String ans){
         if(idx==0){
