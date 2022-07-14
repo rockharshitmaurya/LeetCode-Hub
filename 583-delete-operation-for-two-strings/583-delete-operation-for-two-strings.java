9dae308ca6785efb232bd89d4f6d38c282009d0b
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-07-14 14:21:10 +0530

    Time: 17 ms (43.58%), Space: 47.1 MB (43.28%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.java
@@ -1,14 +1,25 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-     int dp[][] = new int[word1.length()+1][word2.length()+1];
-    for(int i = 0; i <= word1.length(); i++) {
-        for(int j = 0; j <= word2.length(); j++) {
-            if(i == 0 || j == 0) dp[i][j] = 0;
-            else dp[i][j] = (word1.charAt(i-1) == word2.charAt(j-1)) ? dp[i-1][j-1] + 1
-                    : Math.max(dp[i-1][j], dp[i][j-1]);
-        }
+            int val=longestCommonSubsequence(word1,word2);
+        return (word1.length()-val)+(word2.length()-val);
     }
-    int val =  dp[word1.length()][word2.length()];
-    return word1.length() - val + word2.length() - val;   
+        public int longestCommonSubsequence(String text1, String text2) {
+        int n=text1.length(),m=text2.length();
+        int dp[][]=new int[n+1][m+1];
+        for(int i=0; i<=n; i++) dp[i][0]=0;
+        for(int j=0; j<=m; j++) dp[0][j]=0;
+        
+        for(int idx1=1; idx1<=n; idx1++){
+            for(int idx2=1; idx2<=m; idx2++){
+                        if(text1.charAt(idx1-1)==text2.charAt(idx2-1)) dp[idx1][idx2]=1+dp[idx1-1][idx2-1];
+                        else{ 
+                            int left=dp[idx1-1][idx2];
+                            int right=dp[idx1][idx2-1];
+                            dp[idx1][idx2]=Math.max(left,right);
+                        }
+            }
+        }
+        return dp[n][m];
+        
     }
 }
