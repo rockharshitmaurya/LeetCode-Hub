0e255e0aab23bf1e6e14a1ed7e0e752ca1b536d6
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-07-14 23:50:44 +0530

    Time: 8 ms (68.52%), Space: 45 MB (40.92%) - LeetHub

--- 72-edit-distance/72-edit-distance.java
+++ 72-edit-distance/72-edit-distance.java
@@ -3,8 +3,22 @@ class Solution {
     public int minDistance(String word1, String word2) {
         int n=word1.length(),m=word2.length();
         dp=new int[n+1][m+1];
-        for(int arr[]:dp)   Arrays.fill(arr,-1);
-        return helper(word1,word2,n,m);
+        // for(int arr[]:dp)   Arrays.fill(arr,-1);
+        for(int i=0; i<=n; i++) dp[i][0]=i;
+        for(int j=0; j<=m; j++) dp[0][j]=j;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(word1.charAt(i-1)==word2.charAt(j-1))
+                    dp[i][j]=dp[i-1][j-1];
+                else{
+                    int insert=1+dp[i][j-1];
+                    int delete=1+dp[i-1][j];
+                    int replace=1+dp[i-1][j-1];
+                    dp[i][j]=Math.min(insert,Math.min(delete,replace));
+                }
+            }
+        }
+        return dp[n][m];
     }
     int helper(String str1,String str2, int i,int j){
         if(i==0) return j;
