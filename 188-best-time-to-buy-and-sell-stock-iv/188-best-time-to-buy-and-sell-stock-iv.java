560939b4cee63487dab2748d62008d6b052ea8b8
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-07-15 15:06:54 +0530

    Time: 2 ms (90.38%), Space: 42.3 MB (69.84%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,31 +1,33 @@
 class Solution {
     public int maxProfit(int k, int[] prices) {
         int n=prices.length,m=2;
-        int dp[][][]=new int[n+1][m][k+1];
-        for(int buy=0; buy<m; buy++){
-            for(int cap=0; cap<k; cap++){
-                dp[n][buy][cap]=0;
-            }
-        }
-        for(int i=0; i<=n; i++){
-            for(int buy=0; buy<m; buy++){
-                dp[i][buy][0]=0;
-            }
-        }
+        int dp_after[][]=new int[m][k+1];
+        int dp_cur[][]=new int[m][k+1];
+        // for(int buy=0; buy<m; buy++){
+        //     for(int cap=0; cap<k; cap++){
+        //         dp[n][buy][cap]=0;
+        //     }
+        // }
+        // for(int i=0; i<=n; i++){
+        //     for(int buy=0; buy<m; buy++){
+        //         dp[i][buy][0]=0;
+        //     }
+        // }
         for(int i=n-1; i>=0; i--){
             for(int buy=0; buy<=1; buy++){
                 for(int cap=1; cap<=k; cap++){
                         int profit=0;
                         if(buy==1){
-                            profit=Math.max(-prices[i]+dp[i+1][0][cap],0+dp[i+1][1][cap]);
+                            profit=Math.max(-prices[i]+dp_after[0][cap],0+dp_after[1][cap]);
                         }else{
-                            profit=Math.max(prices[i]+dp[i+1][1][cap-1],0+dp[i+1][0][cap]);
+                            profit=Math.max(prices[i]+dp_after[1][cap-1],0+dp_after[0][cap]);
                         }
-                        dp[i][buy][cap]=profit;
+                        dp_cur[buy][cap]=profit;
                     }
             }
+            dp_after=dp_cur;
         }
-        return dp[0][1][k];
+        return dp_after[1][k];
     }
     int helper(int arr[],int cap,int buy,int idx){
         if(idx==arr.length) return 0;
