52e4315a2372121f07cfa701bd0c5327d1f2222e
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-07-16 14:18:26 +0530

    Time: 103 ms (22.16%), Space: 44.1 MB (66.49%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,30 +1,19 @@
 class Solution {
-    
     public int lengthOfLIS(int[] nums) {
-        int n=nums.length;
-       int dp[][]=new int[n+1][n+1];
-        for(int idx=n-1; idx>=0; idx--){
-            for(int prev=idx-1; prev>=-1; prev--){
-                int pick=0;
-                if(prev==-1 || nums[idx]>nums[prev]){
-                   pick=1+dp[idx+1][idx+1];
+        int dp[]=new int[nums.length];
+        Arrays.fill(dp,1); int ans=0;
+        for(int i=0; i<nums.length; i++){
+            int mx=0;
+            for(int j=i-1; j>=0; j--){
+                if(nums[j]<nums[i]){
+                    // dp[i]+=dp[j];
+                    mx=Math.max(mx,dp[j]);
+                    // break;
                 }
-                int notpick=dp[idx+1][prev+1];
-                dp[idx][prev+1]=Math.max(pick,notpick);
             }
+            dp[i]+=mx;
+            ans=Math.max(ans,dp[i]);
         }
-        return dp[0][0];
+        return ans;
     }
-    // int helper(int nums[],int idx,int prev){
-    //     if(idx==nums.length){
-    //         return 0;
-    //     }
-    //     if(dp[idx][prev+1]!=-1) return dp[idx][prev+1];
-    //     int pick=0;
-    //     if(prev==-1 || nums[idx]>nums[prev]){
-    //         pick=1+helper(nums,idx+1,idx);
-    //     }
-    //     int notpick=helper(nums,idx+1,prev);
-    //     return dp[idx][prev+1]=Math.max(pick,notpick);
-    // }
 }
