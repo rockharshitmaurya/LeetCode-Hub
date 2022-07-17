538f820af5c7eadc2b9f61aef8dc2e017b0a0729
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-07-17 12:39:31 +0530

    Time: 270 ms (25.00%), Space: 43.2 MB (87.50%) - LeetHub

--- 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.java
+++ 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.java
@@ -1,30 +1,30 @@
 class Solution {
     public int[] smallestTrimmedNumbers(String[] nums, int[][] queries) {
-        int m=nums.length,n=queries.length;
-        int ans_idx=0,ans[]=new int[n];
-            // Arrays.sort(arr, (a, b)-> (a.val.compareTo(b.val)));
-        for(int arr[]:queries){
-            PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)->(a.val.equals(b.val)?(b.idx-a.idx):(b.val.compareTo(a.val))));
-            for(int i=0; i<m; i++){
-                String num=nums[i].substring(nums[i].length()-arr[1]);
-                pq.add(new Pair(i,num));
-                if(pq.size()>arr[0]) pq.poll();
+        int [] result = new int [queries.length];
+        
+        int idx = 0;
+        
+        Num [] numbers = new Num[nums.length];
+        Num current;
+        
+        for (int [] query : queries){
+            for (int i = 0; i < nums.length; ++i){
+                current = new Num();
+                current.index = i;
+                current.digits = nums[i].substring(nums[i].length() - query[1]);
+                numbers[i] = current;
             }
-            // for(Pair p:pq){
-            //     System.out.print(p.val+"-->"+p.idx+" ");
-            // }
-            // System.out.println();
-            ans[ans_idx++]=pq.poll().idx;
+            
+            Arrays.sort(numbers, (a , b) -> a.digits.compareTo(b.digits));
+            
+            result[idx++] = numbers[query[0] - 1].index;
         }
-        return ans;
-    }
-    class Pair{
-        int idx;
-        String val;
-        public Pair(int idx,String val){
-            this.val=val;
-            this.idx=idx;
-        }
+        
+        return result;
     }
 }
-// 3 2 1
+class Num{
+    String digits;
+    int index;
+    
+}
