00c12bb9930e940b062c754e94249d956881e012
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-07-20 11:15:36 +0530

    Time: 244 ms (26.79%), Space: 121.6 MB (5.85%) - LeetHub

---
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int numMatchingSubseq(String s, String[] words) {
+        Trie root=createTrie(words);
+        return dfs(s,root,0);
+        
+    }
+    private int dfs(String S, Trie root, int pos) {
+        if (root == null) return 0;
+        int index = S.indexOf(root.ch, pos);
+        if (root.ch != '*' && index == -1) return 0;
+        int res = root.score;
+        for (int i = 0; i < 26; i++) {
+            res += dfs(S, root.children[i], root.ch == '*' ? 0 : index + 1);
+        }
+        return res;
+    }
+    class Trie{
+        char ch;
+        int score;
+        Trie children[];
+        public Trie(char ch){
+            this.ch=ch;
+            score=0;
+            children=new Trie[26];
+        }
+    }
+    Trie createTrie(String words[]){
+        Trie root=new Trie('*');
+        for(String str:words){
+            addNode(str,root);
+        }
+        return root;
+    }
+    void addNode(String str,Trie root){
+        for(char ch:str.toCharArray()){
+            if(root.children[ch-'a']==null){
+                root.children[ch-'a']=new Trie(ch);   
+            }
+            root=root.children[ch-'a'];
+        }
+        root.score++;
+    }
+}
