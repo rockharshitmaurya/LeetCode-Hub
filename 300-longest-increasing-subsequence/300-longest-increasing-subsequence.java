c5a5453323ed3b5ea9129d5595d4900ce367c45d
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-07-22 18:15:50 +0530

    Time: 98 ms (25.74%), Space: 44.5 MB (46.90%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,19 +1,29 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int dp[]=new int[nums.length];
+        int hash[]=new int[nums.length];
         Arrays.fill(dp,1); int ans=0;
+        int last_idx=0;
         for(int i=0; i<nums.length; i++){
-            int mx=0;
+            hash[i]=i;
             for(int j=i-1; j>=0; j--){
-                if(nums[j]<nums[i]){
-                    // dp[i]+=dp[j];
-                    mx=Math.max(mx,dp[j]);
-                    // break;
+                if(nums[j]<nums[i] && 1+dp[j]>dp[i]){
+                    dp[i]=1+dp[j];
+                    hash[i]=j;
                 }
             }
-            dp[i]+=mx;
-            ans=Math.max(ans,dp[i]);
+            if(dp[i]>ans){
+                ans=dp[i];
+                last_idx=i;
+            }
         }
+        // String res="";
+        // while(hash[last_idx]!=last_idx){
+        //     res=nums[last_idx]+" "+res;
+        //     last_idx=hash[last_idx];
+        //     // System.out.println("IN");
+        // }
+        // System.out.println(nums[last_idx]+" "+res);
         return ans;
     }
 }
