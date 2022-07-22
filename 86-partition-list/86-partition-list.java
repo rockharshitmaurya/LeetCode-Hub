ca60c7479488e9e757a0bceb50c73c9cc7cab845
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-07-22 09:13:59 +0530

    Time: 1 ms (69.53%), Space: 43.3 MB (6.50%) - LeetHub

--- 86-partition-list/86-partition-list.java
+++ 86-partition-list/86-partition-list.java
@@ -10,23 +10,19 @@
  */
 class Solution {
     public ListNode partition(ListNode head, int x) {
-        ListNode dummy=new ListNode(0);
-        ListNode temp=head,t_head=dummy;
+        ListNode dummy=new ListNode(0),tail=new ListNode(0);
+        ListNode temp=head,t_head=dummy,t_tail=tail;
         while(temp!=null){
             if(temp.val<x){
                 t_head.next=new ListNode(temp.val);
                 t_head=t_head.next;
+            }else{
+                tail.next=new ListNode(temp.val);
+                tail=tail.next;
             }
             temp=temp.next;
         }
-        temp=head;
-        while(temp!=null){
-            if(temp.val>=x){
-                t_head.next=new ListNode(temp.val);
-                t_head=t_head.next;
-            }
-            temp=temp.next;
-        }
+        t_head.next=t_tail.next;
         return dummy.next;
     }
 }
