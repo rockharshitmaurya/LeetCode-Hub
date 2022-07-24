fdbdc30c79428aabf93224a909ebe2dd7063f017
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-07-24 10:31:49 +0530

    Time: 1657 ms (16.67%), Space: 145.9 MB (16.67%) - LeetHub

--- 2353-design-a-food-rating-system/2353-design-a-food-rating-system.java
+++ 2353-design-a-food-rating-system/2353-design-a-food-rating-system.java
@@ -6,7 +6,6 @@ class FoodRatings {
             for(int i=0; i<foods.length; i++){
                 
                 if(!type.containsKey(cui[i])){
-            // PriorityQueue<Pair> p=new PriorityQueue<Pair>((a,b)->b.rt-a.rt==0 ? a.fd.compareTo(b.fd) : b.rt-a.rt);
                     PriorityQueue<Pair> p=new PriorityQueue<Pair>((o1,o2)->( (o1.rt!=o2.rt)?(o2.rt-o1.rt):(o1.fd.compareTo(o2.fd))));
                     type.put(cui[i],p);
                 }
