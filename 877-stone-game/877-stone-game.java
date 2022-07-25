335b94fedd7e9296cc59fa736dd1186ba807c539
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-07-25 18:47:49 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (88.22%) - LeetHub

--- 877-stone-game/877-stone-game.java
+++ 877-stone-game/877-stone-game.java
@@ -1,11 +1,12 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        int sum=0,n=piles.length;
-        for(int num:piles) sum+=num;
-        int dp[][]=new int[n+1][n+1];
-        for(int arr[]:dp) 
-            Arrays.fill(arr,-1);
-        return (sum/2)<=helper(piles,dp,0,n-1);
+        // int sum=0,n=piles.length;
+        // for(int num:piles) sum+=num;
+        // int dp[][]=new int[n+1][n+1];
+        // for(int arr[]:dp) 
+        //     Arrays.fill(arr,-1);
+        // return (sum/2)<=helper(piles,dp,0,n-1);
+        return true;
     }
     int helper(int piles[],int dp[][],int i,int j){
         if(i>=j) return 0;
