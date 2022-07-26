bfbebd1f2f3402c9f9678918ee066ca4337ae37c
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-07-26 15:27:53 +0530

    Time: 7 ms (13.23%), Space: 42.7 MB (20.40%) - LeetHub

--- 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.java
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.java
@@ -1,9 +1,24 @@
 class Solution {
     public int mincostTickets(int[] days, int[] costs) {
         int n=days.length;
-        int dp[][]=new int[n][days[n-1]+40];
-        for(int arr[]:dp) Arrays.fill(arr,-1);
-        return helper(days,costs,0,0,dp);
+        int max=days[n-1]+40;
+        int dp[][]=new int[n+1][max+1];
+        
+        for(int idx=n-1; idx>=0; idx--){
+            for(int limit=max; limit>=0; limit--){
+                if(days[idx]<limit) dp[idx][limit]=dp[idx+1][limit]; 
+                else{
+                    int one_day=costs[0]+dp[idx+1][days[idx]];
+                    int seven_day=costs[1]+dp[idx+1][days[idx]+7];
+                    int thirty_day=costs[2]+dp[idx+1][days[idx]+30];
+                    dp[idx][limit]=Math.min(one_day,Math.min(seven_day,thirty_day));
+                }
+            }
+        }
+        // for(int arr[]:dp) Arrays.fill(arr,-1);
+        // return helper(days,costs,0,0,dp);
+        return dp[0][0];
+        
     }
     int helper(int days[],int costs[],int idx,int limit,int dp[][]){
         if(idx>=days.length) return 0;
