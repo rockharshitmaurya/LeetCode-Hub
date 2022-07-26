fe7382d3c4cb073ba01a952f0710a7892d127ac2
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-07-26 15:31:26 +0530

    Time: 15 ms (5.54%), Space: 48.4 MB (5.01%) - LeetHub

--- 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.java
+++ 983-minimum-cost-for-tickets/983-minimum-cost-for-tickets.java
@@ -1,23 +1,23 @@
 class Solution {
     public int mincostTickets(int[] days, int[] costs) {
         int n=days.length;
-        int max=days[n-1]+40;
-        int dp[][]=new int[n+1][max+1];
+        int max=days[n-1]+30;
+        int dp_prev[]=new int[max+1];
         
         for(int idx=n-1; idx>=0; idx--){
+                int dp_cur[]=new int[max+1];
             for(int limit=max; limit>=0; limit--){
-                if(days[idx]<limit) dp[idx][limit]=dp[idx+1][limit]; 
+                if(days[idx]<limit) dp_cur[limit]=dp_prev[limit]; 
                 else{
-                    int one_day=costs[0]+dp[idx+1][days[idx]];
-                    int seven_day=costs[1]+dp[idx+1][days[idx]+7];
-                    int thirty_day=costs[2]+dp[idx+1][days[idx]+30];
-                    dp[idx][limit]=Math.min(one_day,Math.min(seven_day,thirty_day));
+                    int one_day=costs[0]+dp_prev[days[idx]];
+                    int seven_day=costs[1]+dp_prev[days[idx]+7];
+                    int thirty_day=costs[2]+dp_prev[days[idx]+30];
+                    dp_cur[limit]=Math.min(one_day,Math.min(seven_day,thirty_day));
                 }
             }
+            dp_prev=dp_cur;
         }
-        // for(int arr[]:dp) Arrays.fill(arr,-1);
-        // return helper(days,costs,0,0,dp);
-        return dp[0][0];
+        return dp_prev[0];
         
     }
     int helper(int days[],int costs[],int idx,int limit,int dp[][]){
