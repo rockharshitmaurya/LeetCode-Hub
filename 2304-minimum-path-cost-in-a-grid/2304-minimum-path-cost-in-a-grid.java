7fd8b81eddd1b5ce40d43b3254e51624e1ef6adb
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-07-27 13:04:19 +0530

    Time: 774 ms (5.03%), Space: 120.3 MB (75.92%) - LeetHub

---
+++ 2304-minimum-path-cost-in-a-grid/2304-minimum-path-cost-in-a-grid.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minPathCost(int[][] grid, int[][] moveCost) {
+        int ans=(int)1e9;
+        int m=grid.length,n=grid[0].length;
+        int dp[][]=new int[m][n];
+        for(int i=0; i<grid[0].length; i++){
+            for(int sub[]:dp) Arrays.fill(sub,-1);
+            ans=Math.min(ans,helper(grid,moveCost,0,i,dp));
+        }
+        return ans;
+    }
+    int helper(int[][] grid, int[][] moveCost,int i,int j,int dp[][]){
+        if(i==grid.length-1) return grid[i][j];
+            if(dp[i][j]!=-1) return dp[i][j];
+            int val=(int)1e9;
+            for(int next=0; next<grid[0].length; next++){
+                int node_sum=grid[i][j];
+                int path_sum=moveCost[node_sum][next];
+                val=Math.min(val,node_sum+path_sum+helper(grid,moveCost,i+1,next,dp));
+            }
+        return dp[i][j]=val;
+    }
+}
