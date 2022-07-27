be43d4e387c9c98403488bca4bb18a4caf1195d0
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-07-27 13:17:19 +0530

    Time: 8 ms (90.16%), Space: 120.8 MB (67.16%) - LeetHub

--- 2304-minimum-path-cost-in-a-grid/2304-minimum-path-cost-in-a-grid.java
+++ 2304-minimum-path-cost-in-a-grid/2304-minimum-path-cost-in-a-grid.java
@@ -1,13 +1,29 @@
 class Solution {
     public int minPathCost(int[][] grid, int[][] moveCost) {
-        int ans=(int)1e9;
         int m=grid.length,n=grid[0].length;
         int dp[][]=new int[m][n];
-        for(int i=0; i<grid[0].length; i++){
-            for(int sub[]:dp) Arrays.fill(sub,-1);
-            ans=Math.min(ans,helper(grid,moveCost,0,i,dp));
+        for(int sub[]:dp) Arrays.fill(sub,(int)1e9);
+        
+        for(int i=0; i<n; i++) dp[m-1][i]=grid[m-1][i];
+        
+        for(int i=m-2; i>=0; i--){
+            for(int j=0; j<n; j++){
+                    int val=(int)1e9;
+                    for(int next=0; next<grid[0].length; next++){
+                        int node_sum=grid[i][j];
+                        int path_sum=moveCost[node_sum][next];
+                        val=Math.min(val,node_sum+path_sum+dp[i+1][next]);
+                    }
+                dp[i][j]=val;
+            }
         }
-        return ans;
+        
+        // for(int i=0; i<grid[0].length; i++){
+        //     for(int sub[]:dp) Arrays.fill(sub,-1);
+        //     ans=Math.min(ans,helper(grid,moveCost,0,i,dp));
+        // }
+        for(int i=0; i<n; i++) dp[0][0]=Math.min(dp[0][0],dp[0][i]);
+        return dp[0][0];
     }
     int helper(int[][] grid, int[][] moveCost,int i,int j,int dp[][]){
         if(i==grid.length-1) return grid[i][j];
