bacf09bafdfdc5714c2f009778620d2ef0d37b10
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-07-28 12:37:56 +0530

    Time: 2 ms (92.48%), Space: 68.7 MB (17.89%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,28 +1,8 @@
 class Solution {
-    public boolean canJump(int[] nums) {
-        int n=nums.length;
-        boolean dp[]=new boolean[n];
-        dp[n-1]=true;
-        
-        for(int idx=n-2; idx>=0; idx--){
-            boolean jump=false; 
-            if(nums[idx]==0) continue;
-            for(int i=idx+1; i<=Math.min((idx+nums[idx]),n-1); i++)
-                jump|=dp[i];
-            dp[idx]=jump;
-        }
-        
-        return dp[0];
-    }
-    
-    boolean helper(int nums[],int idx){
-        if(idx==nums.length-1) return true;
-        if(idx>=nums.length) return false;
-        if(nums[idx]==0) return false;
-        boolean jump=false;
-        
-        for(int i=idx+1; i<=(idx+nums[idx]); i++)
-            jump|=helper(nums,i);
-        return jump;
+    public boolean canJump(int[] A) {
+        int i = 0 , n = A.length;
+        for (int reach = 0; i < n && i <= reach; ++i)
+            reach = Math.max(i + A[i], reach);
+        return i == n;
     }
 }
