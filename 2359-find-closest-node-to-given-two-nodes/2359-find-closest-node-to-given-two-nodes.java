3745c55b70068580e6f81c014d1a38ca0573f61c
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-07-31 16:20:39 +0530

    Time: 39 ms (50.00%), Space: 110.8 MB (50.00%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.java
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.java
@@ -5,23 +5,15 @@ class Solution {
         int arr_2[]=new int[n]; Arrays.fill(arr_2,-1);
         dfs(edges,node1,0,arr_1,new boolean[n]);
         dfs(edges,node2,0,arr_2,new boolean[n]);
-        // System.out.println(Arrays.toString(arr_1));
-        // System.out.println(Arrays.toString(arr_2));
         int idx=-1,temp=Integer.MAX_VALUE;
         for(int i=0; i<n; i++){
             if(arr_1[i]!=-1 && arr_2[i]!=-1){
-                if((arr_1[i]+arr_2[i])<temp){
-                    temp=(arr_1[i]+arr_2[i]);
+                if(Math.max(arr_1[i],arr_2[i])<temp){
+                    temp=Math.max(arr_1[i],arr_2[i]);
                     idx=i;
-                    // return idx;
                 }
             }
         }
-        if(edges[0]==4 && edges[n-1]==1){
-        String key="[4, 4, 8, -1, 9, 8, 4, 4, 1, 1]";
-        return key.equals(Arrays.toString(edges))?1:idx;
-        }
-        // System.out.println(Arrays.toString(edges));
         return idx;
     }
     void dfs(int edges[],int root,int len,int dis[],boolean vis[]){
