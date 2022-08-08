eb78752ab4638367bef074990ce3193f9ea5cdad
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-08-08 11:40:10 +0530

    Time: 81 ms (47.24%), Space: 48.7 MB (12.11%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -5,9 +5,11 @@ class Solution {
         int size=0;
         for(int i=0; i<n; i++){
             int idx=Arrays.binarySearch(dp,0,size,nums[i]);
-            if(idx < 0) idx = -(idx + 1);
+            if(idx < 0) idx = -(idx + 1); //trick copied
             dp[idx]=nums[i];
+            System.out.print(idx+" ");
             if(idx==size) size++;
+            
         }
         return size;
     }
