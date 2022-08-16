0ecbc2dbcf93ebf5833dc0de0044dc2cd15f86db
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-08-16 10:11:21 +0530

    Time: 10 ms (84.43%), Space: 50.2 MB (48.95%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
@@ -3,24 +3,10 @@ class Solution {
         int count[]=new int[26];
         
         for(char ch:s.toCharArray()){
-            if(count[ch-'a']==-1 || count[ch-'a']>0){
-                count[ch-'a']=-1;
-            }else{
                 count[ch-'a']++;
-            }
         }
-//         char ch='1';
-//         for(int i=0; i<26; i++){
-//             if(count[i]>0){
-//                 ch = (char) (i+'a');
-//                 break;
-//             }
-//         }
-        
-//         if(ch=='1') return -1;
-        // System.out.println()
         for(int i=0; i<s.length(); i++){
-            if(count[s.charAt(i)-'a']>0) return i;
+            if(count[s.charAt(i)-'a']==1) return i;
         }
         return -1;
     }
