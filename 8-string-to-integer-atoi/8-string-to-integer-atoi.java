2a2532ed2b9ae601aedec9bbb2f761eb205e8f27
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-08-17 01:33:31 +0530

    Time: 12 ms (13.94%), Space: 42.8 MB (71.95%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.java
@@ -12,8 +12,8 @@ class Solution {
         }
         
 
-         if(sb.length()==1 && !Character.isDigit(sb.charAt(0))) return 0;
-        if(sb.length()==0) return 0;
+        //  if(sb.length()==1 && !Character.isDigit(sb.charAt(0))) return 0;
+        // if(sb.length()==0) return 0;
         
         for(int i=0; i<sb.length(); i++){
             char ch=s.charAt(i);
