624023587cfecee2231f8cc64cf0ccd946efe5be
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-08-17 01:39:55 +0530

    Time: 8 ms (18.39%), Space: 40.5 MB (99.93%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.java
@@ -6,23 +6,22 @@ class Solution {
         for(int i=0; i<s.length(); i++){
             char ch=s.charAt(i);
            if(((i==0) && (ch=='-' || ch=='+')) || Character.isDigit(ch)){
-               sb.append(ch);
+                 if(ch>'0') flag=true;
+                if(((i==0) && (ch=='-' || ch=='+')) || flag) sb_2.append(ch); 
+               // sb.append(ch);
            } 
             else break;
         }  
-        for(int i=0; i<sb.length(); i++){
-            char ch=s.charAt(i);
-            if((i==0) && (ch=='-' || ch=='+')) sb_2.append(ch);
-            else{
-                if(ch>'0') flag=true;
-                
-                if(flag) sb_2.append(ch);
-                
-            }
-            
-            
-        }
-        // System.out.println(sb_2);
+        // for(int i=0; i<sb.length(); i++){
+        //     char ch=s.charAt(i);
+        //     if((i==0) && (ch=='-' || ch=='+')) sb_2.append(ch);
+        //     else{
+        //         if(ch>'0') flag=true;
+        //         if(flag) sb_2.append(ch);   
+        //     }    
+        // }
+        // sb_2=sb;
+        
         if(sb_2.length()==1 && !Character.isDigit(sb_2.charAt(0))) return 0;
         if(sb_2.length()==0) return 0;
         if(sb_2.length()>12){
