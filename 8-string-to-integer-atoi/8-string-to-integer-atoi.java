c891985beefb971d99c63c153de7fc2f73971a21
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-08-17 01:31:58 +0530

    Time: 10 ms (15.49%), Space: 42.7 MB (77.13%) - LeetHub

---
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int myAtoi(String s) {
+        StringBuilder sb=new StringBuilder(),sb_2=new StringBuilder();
+        s=s.trim();
+        boolean flag=false;
+        for(int i=0; i<s.length(); i++){
+            char ch=s.charAt(i);
+           if(((i==0) && (ch=='-' || ch=='+')) || Character.isDigit(ch)){
+               sb.append(ch);
+           } 
+            else break;
+        }
+        
+
+         if(sb.length()==1 && !Character.isDigit(sb.charAt(0))) return 0;
+        if(sb.length()==0) return 0;
+        
+        for(int i=0; i<sb.length(); i++){
+            char ch=s.charAt(i);
+            if((i==0) && (ch=='-' || ch=='+')) sb_2.append(ch);
+            else{
+                if(ch>'0') flag=true;
+                
+                if(flag) sb_2.append(ch);
+                
+            }
+            
+            
+        }
+        System.out.println(sb_2);
+        if(sb_2.length()==1 && !Character.isDigit(sb_2.charAt(0))) return 0;
+        if(sb_2.length()==0) return 0;
+        if(sb_2.length()>12){
+            sb_2.setLength(12);
+        }
+        
+        s=sb_2.toString();
+        long num=Long.parseLong(s);
+        if(num>Integer.MAX_VALUE) return Integer.MAX_VALUE;
+        if(num<Integer.MIN_VALUE) return Integer.MIN_VALUE;
+        return (int)num;  
+        // return 1;
+    }
+}
