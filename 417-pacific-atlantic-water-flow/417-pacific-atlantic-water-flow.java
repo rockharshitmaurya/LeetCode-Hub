8b318fe1f13d4af42513ec1c6d3c148dd7e8e16f
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-08-31 15:20:56 +0530

    Time: 76 ms (13.05%), Space: 64.3 MB (12.36%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
@@ -1,23 +1,20 @@
 class Solution {
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
         List<List<Integer>> ans=new ArrayList<>();
-        boolean vis[][]=new boolean[heights.length][heights[0].length];
         for(int i=0; i<heights.length; i++){
             for(int j=0; j<heights[0].length; j++){
-                if(dfs(heights,i,j,heights[i][j],new HashSet<>(),vis).size()==2){
-                    vis[i][j]=true;
+                if(dfs(heights,i,j,heights[i][j],new HashSet<>()).size()==2){
+
                     ArrayList<Integer> temp=new ArrayList<>();
                     temp.add(i); temp.add(j); ans.add(temp);
                 }
-                // if(i==0 && j==0) System.out.println("0,0 End");
+                
             }
         }
-        // System.out.println(Arrays.deepToString(heights));
         return ans;
     }
-    
-    HashSet<String> dfs(int[][] heights,int i,int j,int num,HashSet<String> set,boolean vis[][]){
-        // System.out.println(i+" "+j);
+ 
+    HashSet<String> dfs(int[][] heights,int i,int j,int num,HashSet<String> set){
         if(set.size()==2) return set;
         if(i<0 || j<0){
             set.add("Pacific");
@@ -26,17 +23,13 @@ class Solution {
             set.add("Atlantic");
             return set;
         }
-        // if(heights[i][j]!=-1 && heights[i][j]<=num && vis[i][j]==true){
-        //     set.add("A"); set.add("P");
-        //     return set;
-        // }        
         if(heights[i][j]!=-1 && heights[i][j]<=num){
             int store=heights[i][j];
             heights[i][j]=-1;
-            dfs(heights,i,j+1,store,set,vis);
-            dfs(heights,i+1,j,store,set,vis);
-            dfs(heights,i,j-1,store,set,vis);
-            dfs(heights,i-1,j,store,set,vis);
+            dfs(heights,i,j+1,store,set);
+            dfs(heights,i+1,j,store,set);
+            dfs(heights,i,j-1,store,set);
+            dfs(heights,i-1,j,store,set);
             heights[i][j]=store;
         }  
         return set;
