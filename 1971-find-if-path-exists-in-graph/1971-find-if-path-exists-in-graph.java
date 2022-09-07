507c8c591b7c53a3dbf67a6b66fd7c7605306a6f
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-09-07 16:47:42 +0530

    Time: 12 ms (97.52%), Space: 138.1 MB (84.54%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,45 +1,27 @@
-class DisjointSetUnion{
-    private int[] parent;
-    private int N;
-    
-    public DisjointSetUnion(int n){
-        this.N = n;
-        this.parent = new int[this.N];
-        for(int i = 0; i < this.N; i++){
-            this.parent[i] = i;
-        }
-    }
-    
-    public boolean areConnected(int u, int v){
-        return find(u) == find(v);
-    }
-    
-    public void union(int u, int v){
-        // if(u != v){
-            int a = find(u);
-            int b = find(v);
-            parent[a] = b;
-        // }
-    }
-    
-    private int find(int u){
-        int x = u;
-        while(x != this.parent[x]){
-            x = this.parent[x];
-        }
-        
-        this.parent[u] = x;
-        return x;
-    }
-}
-
 class Solution {
-    public boolean validPath(int n, int[][] edges, int start, int end) {
-        DisjointSetUnion set = new DisjointSetUnion(n);
-        for(int[] edge : edges){
-            set.union(edge[0], edge[1]);
+    int parent[];
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        parent=new int[n];
+        for(int i=0; i<n; i++){
+            parent[i]=i;
+        } 
+        
+        for(int arr[]:edges){
+            union(arr[0],arr[1]);
         }
         
-        return set.areConnected(start, end);
+        return find(source)==find(destination);
+    }
+    
+    
+    void union(int u,int v){
+        int a=find(u);
+        int b=find(v);
+        parent[a]=b;
+    }
+    
+    int find(int root){
+        if(root==parent[root]) return root;
+        return parent[root]=find(parent[root]);
     }
 }
