a8c8eb728bd4e79972efe14b4bae1b690f886c82
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-09-10 01:05:16 +0530

    Time: 252 ms (9.97%), Space: 166.1 MB (10.45%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.java
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.java
@@ -1,33 +1,17 @@
 class Solution {
-    public int numberOfWeakCharacters(int[][] properties) {
-//         int ans=0;
-//         Arrays.sort(pro,(o1,o2)->((o1[0]==o2[0])?(o1[1]-o2[1]):(o1[0]-o2[0])));
+    public int numberOfWeakCharacters(int[][] pro) {
+        int ans=0;
+        Arrays.sort(pro,(o1,o2)->((o1[0]==o2[0])?(o1[1]-o2[1]):(o2[0]-o1[0])));
+        Stack<int[]> st=new Stack<>();
         
-//         System.out.println(Arrays.deepToString(pro));
-//         for(int i=0; i<pro.length; i++){
-//             for(int j=i+1; j<pro.length; j++){
-//                 if(pro[i][0]<pro[j][0] && pro[i][1]<pro[j][1]){
-                    
-//                     System.out.println(pro[i][0]+" "+pro[i][1]+"-->"+pro[j][0]+" "+pro[j][1]);
-//                     ans++;
-//                 }
-//             }
-//         }
         
-//         return ans;
-        int n = properties.length;
-        int count = 0;
+        st.push(pro[0]);
         
-        Arrays.sort(properties, (a, b) -> (b[0] == a[0]) ? (a[1] - b[1]) : b[0] - a[0]);
-        
-        int max = 0;
-        for (int i = 0; i < n; i++) {
-            
-            if (properties[i][1] < max) {
-                count++;
-            }
-            max = Math.max(max, properties[i][1]);
+        for(int i=1; i<pro.length; i++){
+            if(pro[i][0]<st.peek()[0] && pro[i][1]<st.peek()[1]) ans++;
+            else st.push(pro[i]);
         }
-        return count;
+        
+        return ans;
     }
 }
