c977c0f96634913d4dfb0bcf42419129f31b85b8
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-09-16 23:22:48 +0530

    Time: 65 ms (82.93%), Space: 95.3 MB (85.07%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.java
@@ -3,18 +3,20 @@ class Solution {
     public int maximumScore(int[] nums, int[] multi) {
         int m = multi.length;
         int n = nums.length;
-        int dp[][]=new int[m+1][m+1];
+        int dp_cur[]=new int[m+1];
         
         
         for(int idx=m-1; idx>=0; idx--){
+            int dp_next[]=new int[m+1];
             for(int left=idx; left>=0; left--){
-                int front=nums[left]*multi[idx]+dp[left+1][idx+1];
-                int end=nums[n-1-(idx-left)]*multi[idx]+dp[left][idx+1];
-                 dp[left][idx]=Math.max(front,end);  
+                int front=nums[left]*multi[idx]+dp_cur[left+1];
+                int end=nums[n-1-(idx-left)]*multi[idx]+dp_cur[left];
+                 dp_next[left]=Math.max(front,end);  
             }
+            dp_cur=dp_next;
         }
         
-        return dp[0][0];
+        return dp_cur[0];
         
     }
     
