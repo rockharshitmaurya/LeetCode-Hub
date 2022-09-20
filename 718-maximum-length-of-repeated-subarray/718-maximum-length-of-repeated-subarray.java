af66e06336cbab8912a5a212795f69e2064a2b13
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-09-20 16:08:12 +0530

    Time: 96 ms (34.32%), Space: 75.8 MB (71.13%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
@@ -1,18 +1,20 @@
 class Solution {
     public int findLength(int[] nums1, int[] nums2) {
         int n=nums1.length,m=nums2.length;
-        int dp[][]=new int[n+1][m+1];
+        int dp_cur[]=new int[m+1],dp_prev[]=new int[m+1];
         
         int ans=0;
         for(int i=1; i<=n; i++){
+            dp_cur=new int[m+1];
             for(int j=1; j<=m; j++){
                 if(nums1[i-1]==nums2[j-1]){
-                    dp[i][j]=1+dp[i-1][j-1]; // if the prev char matches then the current res will be length of prev match and +1 for the current match 
-                    ans=Math.max(ans,dp[i][j]);
+                    dp_cur[j]=1+dp_prev[j-1]; // if the prev char matches then the current res will be length of prev match and +1 for the current match 
+                    ans=Math.max(ans,dp_cur[j]);
                 }else{
-                    dp[i][j]=0; // if not match then  the cur ans will be zero
+                    dp_cur[j]=0; // if not match then  the cur ans will be zero
                 }
             }
+            dp_prev=dp_cur;
         }
         
         return ans;
