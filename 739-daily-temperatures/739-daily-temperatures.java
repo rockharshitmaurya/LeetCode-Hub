7a614ce0098b99a8600efa59d297930383c3f94b
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-09-24 23:17:33 +0530

    Time: 212 ms (20.44%), Space: 131.5 MB (44.63%) - LeetHub

---
+++ 739-daily-temperatures/739-daily-temperatures.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temp) {
+        Stack<Integer> st=new Stack<>();
+        
+        int n=temp.length;
+        int ans[]=new int[n];
+        for(int i=n-1; i>=0; i--){
+            while(!st.isEmpty() && temp[st.peek()]<=temp[i]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                ans[i]=0;
+            }else{
+                ans[i]=st.peek()-i;
+            }
+            
+            st.push(i);
+        }
+        
+        return ans;
+    }
+}
