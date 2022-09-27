82e440072bfc2f93f4b8989d7c3eba1048866737
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-09-27 22:23:44 +0530

    Time: 76 ms (12.40%), Space: 139.8 MB (31.90%) - LeetHub

---
+++ 1905-count-sub-islands/1905-count-sub-islands.java
@@ -0,0 +1,69 @@
+class Solution {
+    int n,m;
+    int parent[];
+    boolean res;
+    int gridCopy[][];
+    void union(int x,int y){
+        int a=find(x);
+        int b=find(y);
+        parent[a]=b;
+    }
+    
+    int find(int x){
+        if(parent[x]==x) return x;
+        return parent[x]=find(parent[x]);
+    }
+    public int countSubIslands(int[][] grid1, int[][] grid2) {
+      // this.grid1=grid1;
+        gridCopy=grid1;
+        n=grid1.length;m=grid1[0].length; 
+      parent=new int[(n*m)+1001];
+      for(int i=0; i<=(n*m)+1000; i++){
+          parent[i]=i;
+      }
+      
+      for(int i=0; i<n; i++){
+          for(int j=0; j<m; j++){
+              if(grid1[i][j]==1) helperGrid1(grid1,i,j,i*n+j);
+          }
+      }
+     int ans=0;
+       
+     for(int i=0; i<n; i++){
+          for(int j=0; j<m; j++){
+              if(grid2[i][j]==1) {
+                  res=true;
+                  helperGrid2(grid2,i,j,i*n+j);
+                  if(res){
+                      // System.out.println(i+" "+j);
+                      ans++;
+                  }
+              }
+          }
+      }
+      return ans;
+    }
+    
+    void helperGrid1(int[][] grid1,int i,int j,int prev){
+        if(i>=0 && i<n && j>=0 && j<m && grid1[i][j]==1){
+            // union(prev,i*n+j);
+            grid1[i][j]=2;
+            helperGrid1(grid1,i+1,j,i*n+j);
+            helperGrid1(grid1,i-1,j,i*n+j);
+            helperGrid1(grid1,i,j+1,i*n+j);
+            helperGrid1(grid1,i,j-1,i*n+j);
+            // grid1[i][j]=1;
+        }
+    }
+    void helperGrid2(int[][] grid2,int i,int j,int prev){
+        if(i>=0 && i<n && j>=0 && j<m && grid2[i][j]==1){
+            // res=res & (find(prev)==find(i*n+j));
+            res=res & (gridCopy[i][j]==2);
+            grid2[i][j]=0;
+            helperGrid2(grid2,i+1,j,i*n+j);
+            helperGrid2(grid2,i-1,j,i*n+j);
+            helperGrid2(grid2,i,j+1,i*n+j);
+            helperGrid2(grid2,i,j-1,i*n+j);
+        }
+    }
+}
