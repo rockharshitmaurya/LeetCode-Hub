311be17cb438c5e4d6eae9769264c821850f543d
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-10-02 01:56:10 +0530

    Time: 264 ms (16.67%), Space: 78.1 MB (83.33%) - LeetHub

---
+++ 2426-number-of-pairs-satisfying-inequality/2426-number-of-pairs-satisfying-inequality.java
@@ -0,0 +1,29 @@
+
+class Solution {
+    public long numberOfPairs(int[] nums1, int[] nums2, int diff) {
+        ArrayList<Integer> sortedList=new ArrayList<>();
+        int n=nums1.length;
+        long ans=0;
+        for(int i=0; i<n; i++){
+            int ele=nums1[i]-nums2[i]+diff;
+            ans+=countSmallerEqual(sortedList,ele+diff);
+            sortedList.add((int)countSmallerEqual(sortedList,ele),ele);
+        }
+        
+        return ans;
+    }
+    
+    private long countSmallerEqual(List<Integer> diffList, int target) {
+        int left = 0, right = diffList.size(); 
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (diffList.get(mid) <= target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return (long)left;
+    }
+
+}
