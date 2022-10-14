e95b12a9bc8646878f0a08e270d0c448fc8d248f
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-10-14 14:37:42 +0530

    Time: 3 ms (17.21%), Space: 48.3 MB (28.60%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        // if(image[])
+        Queue<int[]> q=new LinkedList<>();
+        int n=image.length,m=image[0].length;
+        q.offer(new int[]{sr,sc});
+        int dirs[][]={{0,1},{1,0},{-1,0},{0,-1}};
+        while(!q.isEmpty()){
+            int x=q.peek()[0];
+            int y=q.poll()[1];
+            int val=image[x][y];
+            image[x][y]=-1;
+            for(int arr[]:dirs){
+                int nrow=x+arr[0];
+                int ncol=y+arr[1];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && image[nrow][ncol]==val && image[nrow][ncol]!=-1){
+                    q.offer(new int[]{nrow,ncol});
+                }
+            }
+        }
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(image[i][j]==-1) image[i][j]=color;
+            }
+        }
+        return image;
+    }
+}
