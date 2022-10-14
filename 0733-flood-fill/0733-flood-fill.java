bcaefe809685243591f1715ad7282f05934a3f50
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-10-14 14:38:51 +0530

    Time: 1 ms (91.01%), Space: 42.7 MB (99.33%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        // if(image[])
+        if(image[sr][sc]==color) return image;
         Queue<int[]> q=new LinkedList<>();
         int n=image.length,m=image[0].length;
         q.offer(new int[]{sr,sc});
@@ -9,20 +9,20 @@ class Solution {
             int x=q.peek()[0];
             int y=q.poll()[1];
             int val=image[x][y];
-            image[x][y]=-1;
+            image[x][y]=color;
             for(int arr[]:dirs){
                 int nrow=x+arr[0];
                 int ncol=y+arr[1];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && image[nrow][ncol]==val && image[nrow][ncol]!=-1){
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && image[nrow][ncol]==val && image[nrow][ncol]!=color){
                     q.offer(new int[]{nrow,ncol});
                 }
             }
         }
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(image[i][j]==-1) image[i][j]=color;
-            }
-        }
+        // for(int i=0; i<n; i++){
+        //     for(int j=0; j<m; j++){
+        //         if(image[i][j]==-1) image[i][j]=color;
+        //     }
+        // }
         return image;
     }
 }
