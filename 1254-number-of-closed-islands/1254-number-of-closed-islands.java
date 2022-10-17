deb431896128aac8958f86c0366919a1b6a746d2
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-10-17 16:37:59 +0530

    Time: 7 ms (33.87%), Space: 46.9 MB (42.44%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.java
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.java
@@ -6,32 +6,31 @@ class Solution {
 
     public int solve(int[][] board) {
         int n = board.length, m = board[0].length;
-        boolean vis[][] = new boolean[n][m];
         for (int i = 0; i < n; i++) {
-            if (board[i][0] == 0) dfs(board, vis, i, 0);
-            if (board[i][m - 1] == 0) dfs(board, vis, i, m - 1);
+            if (board[i][0] == 0) dfs(board, i, 0);
+            if (board[i][m - 1] == 0) dfs(board, i, m - 1);
         }
         for (int i = 0; i < m; i++) {
-            if (board[0][i] == 0) dfs(board, vis, 0, i);
-            if (board[n - 1][i] == 0) dfs(board, vis, n - 1, i);
+            if (board[0][i] == 0) dfs(board,  0, i);
+            if (board[n - 1][i] == 0) dfs(board, n - 1, i);
         }
         int ans = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (board[i][j] == 0 && vis[i][j] == false) {
-                    dfs(board,vis,i,j); ans++;
+                if (board[i][j] == 0 ) {
+                    dfs(board,i,j); ans++;
                 }
             }
         }
         return ans;
     }
 
-    void dfs(int board[][], boolean vis[][], int i, int j) {
-        if (i < 0 || j < 0 || i == board.length || j == board[0].length || vis[i][j] || board[i][j] == 1) return;
-        vis[i][j] = true;
-        dfs(board, vis, i + 1, j);
-        dfs(board, vis, i - 1, j);
-        dfs(board, vis, i, j + 1);
-        dfs(board, vis, i, j - 1);
+    void dfs(int board[][], int i, int j) {
+        if (i < 0 || j < 0 || i == board.length || j == board[0].length || board[i][j] == 1) return;
+        board[i][j]=1;
+        dfs(board, i + 1, j);
+        dfs(board,  i - 1, j);
+        dfs(board,  i, j + 1);
+        dfs(board,  i, j - 1);
     }
 }
