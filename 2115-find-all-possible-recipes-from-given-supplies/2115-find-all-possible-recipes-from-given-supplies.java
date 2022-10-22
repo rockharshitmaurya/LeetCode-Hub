56206782bc90739ab48555de0ef0ca0cf9311e6f
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-10-23 00:33:39 +0530

    Time: 617 ms (13.41%), Space: 74.2 MB (60.25%) - LeetHub

---
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.java
@@ -0,0 +1,30 @@
+class Solution {
+
+    public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] sup) {
+        List<String> ans = new ArrayList<>();
+
+        HashSet<String> set = new HashSet<>();
+        HashSet<Integer> seen = new HashSet<>();
+
+        for (String str : sup) set.add(str);
+        boolean flag = true;
+
+        while (flag) {
+            flag = false;
+            outer:for (int i = 0; i < recipes.length; i++) {
+                if (seen.contains(i)) continue;
+                String food = recipes[i];
+
+                for (String ind : ingredients.get(i)) {
+                    if (!set.contains(ind)) continue outer;
+                }
+                ans.add(food);
+                set.add(food);
+                seen.add(i);
+                flag = true;
+            }
+        }
+
+        return ans;
+    }
+}
