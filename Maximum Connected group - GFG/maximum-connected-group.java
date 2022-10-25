fbd263ffcf62d6c8fdd1b8917ce9c5fbabb221a7
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-10-25 16:26:07 +0530

    Added solution - LeetHub

---
+++ Maximum Connected group - GFG/maximum-connected-group.java
@@ -0,0 +1,122 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int[][] grid = new int[n][n];
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    grid[i][j]=sc.nextInt();
+                }
+            }
+            
+            Solution obj = new Solution();
+            int ans = obj.MaxConnection(grid);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    int parent[];
+    void union(int u,int v){
+        int a=find(u);
+        int b=find(v);
+        parent[b]=a;
+    }
+    int find(int x){
+        if(parent[x]==x) return x;
+        return parent[x]=find(parent[x]);
+    }
+    HashMap<Integer,Integer> map=new HashMap<>(); 
+    int score=0;
+    public int MaxConnection(int grid[][]) {
+        
+        int n=grid[0].length,m=grid[0].length;
+        parent=new int[n*m];
+        for(int i=0; i<n*m; i++){
+            parent[i]=i;
+        }
+        
+        boolean vis[][]=new boolean[n][m];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(vis[i][j]) continue;
+                
+                if(grid[i][j]==1){
+                   score=0;
+                   
+                   helper(i,j,vis,grid,(i*m)+j); 
+                   map.put((i*m)+j,score);
+                }
+            }
+        }
+        
+        // System.out.println(find(0)+" "+find(8)+" "+find(5)+" "+find(2));
+        // union(0,1);
+        // System.out.println(find(1)+" "+find(2));
+        int ans=1;
+        int dirs[][]={{0,1},{1,0},{-1,0},{0,-1}};
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(grid[i][j]==0){
+                    int res=1;
+                    HashSet<Integer> set=new HashSet<>();
+                    for(int arr[]:dirs){
+                        int row=i+arr[0];
+                        int col=j+arr[1];
+                        if(row<0 || col<0 || row==n || col==m || grid[row][col]==0) continue;
+                        
+                        int root= (i * m) + j;
+                        int node= (row * m) + col;
+                        // System.out.println(find(root)+"-->"+find(node));
+                        
+                         
+                         int key=find(node);
+                         
+                         if(set.contains(key)) continue;
+                        //  System.out.println(node+"--->"+key);
+                        // System.out.println(key);
+                         res  = res + map.get(key);
+                        //  union(key,root);
+                        set.add(key);
+                         
+                        //  if(find(root)!=key){
+                        //      System.out.println(node+"<-->"+key);
+                        //  }
+                        
+                        // System.out.println(i+"-->"+j+"=="+row+"-->"+col+" ans : "+res);
+                    }
+                    
+                    ans = Math.max (ans , res);
+                }
+            }
+        }
+        // System.out.println(map);
+        return ans;
+    }
+    
+    void helper(int i,int j,boolean vis[][],int grid[][],int root){
+        if(i<0 || j<0 || i==vis.length || j==vis[0].length || grid[i][j]==0 || vis[i][j]) return;
+                
+            vis[i][j]=true;
+            score++;
+            union(root,(i*grid[0].length)+j);
+        
+            helper(i+1,j,vis,grid,root);
+            helper(i,j+1,vis,grid,root);
+            helper(i-1,j,vis,grid,root);
+            helper(i,j-1,vis,grid,root);
+        
+    }
+    
+}
