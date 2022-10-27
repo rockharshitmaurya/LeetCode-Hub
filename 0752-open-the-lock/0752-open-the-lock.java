8d2ad09629e433d4357892d630818d105a9798ac
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-10-27 15:09:40 +0530

    Time: 217 ms (64.56%), Space: 118.3 MB (27.09%) - LeetHub

--- 0752-open-the-lock/0752-open-the-lock.java
+++ 0752-open-the-lock/0752-open-the-lock.java
@@ -10,30 +10,32 @@ class Solution {
     public int openLock(String[] deadends, String target) {
         HashSet<String> set=new HashSet<>();
         for(String word:deadends) set.add(word);
-        if(set.contains("0000")) return -1;
+
         Queue<Pair> q=new LinkedList<>();
         q.offer(new Pair("0000",0));
         
         
         while(!q.isEmpty()){
             Pair p=q.poll();
+            if(set.contains(p.word)) continue;
             if(p.word.equals(target)) return p.len;
+            set.add(p.word);
             
             StringBuilder sb=new StringBuilder(p.word);
             
             for(int i=0; i<4; i++){
                 char cur=sb.charAt(i);
                 sb.setCharAt(i,inc(cur));
-                if(!set.contains(sb.toString())){
+                // if(!set.contains(sb.toString())){
                     q.offer(new Pair(sb.toString(),p.len+1));
-                    set.add(sb.toString());
-                }
+                    // set.add(sb.toString());
+                // }
                 
                 sb.setCharAt(i,dec(cur));
-                if(!set.contains(sb.toString())){
+                // if(!set.contains(sb.toString())){
                     q.offer(new Pair(sb.toString(),p.len+1));
-                    set.add(sb.toString());
-                }
+                    // set.add(sb.toString());
+                // }
                 
                 sb.setCharAt(i,cur);
             }
