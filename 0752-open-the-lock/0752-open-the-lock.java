520d8806a9e3d403e1d46274e72a459e34707b5b
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-10-27 15:07:11 +0530

    Time: 223 ms (62.54%), Space: 92.7 MB (55.05%) - LeetHub

---
+++ 0752-open-the-lock/0752-open-the-lock.java
@@ -0,0 +1,52 @@
+class Solution {
+    class Pair{
+        String word;
+        int len=0;
+        Pair(String w,int l){
+            word=w;
+            len=l;
+        }
+    }
+    public int openLock(String[] deadends, String target) {
+        HashSet<String> set=new HashSet<>();
+        for(String word:deadends) set.add(word);
+        if(set.contains("0000")) return -1;
+        Queue<Pair> q=new LinkedList<>();
+        q.offer(new Pair("0000",0));
+        
+        
+        while(!q.isEmpty()){
+            Pair p=q.poll();
+            if(p.word.equals(target)) return p.len;
+            
+            StringBuilder sb=new StringBuilder(p.word);
+            
+            for(int i=0; i<4; i++){
+                char cur=sb.charAt(i);
+                sb.setCharAt(i,inc(cur));
+                if(!set.contains(sb.toString())){
+                    q.offer(new Pair(sb.toString(),p.len+1));
+                    set.add(sb.toString());
+                }
+                
+                sb.setCharAt(i,dec(cur));
+                if(!set.contains(sb.toString())){
+                    q.offer(new Pair(sb.toString(),p.len+1));
+                    set.add(sb.toString());
+                }
+                
+                sb.setCharAt(i,cur);
+            }
+            
+        }
+        
+        return -1;
+    }
+    
+    char inc(char ch){
+        return ch=='9'?'0':++ch;
+    }
+    char dec(char ch){
+        return ch=='0'?'9':--ch;
+    }
+}
