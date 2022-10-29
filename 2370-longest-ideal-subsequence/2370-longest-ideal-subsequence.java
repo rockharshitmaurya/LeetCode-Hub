047d979cb8f50b1ba4263ab990512e12748acd89
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-10-29 18:36:23 +0530

    Time: 854 ms (5.27%), Space: 310.6 MB (5.02%) - LeetHub

--- 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.java
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.java
@@ -1,16 +1,19 @@
 class Solution {
+    int helper_function(String s,int k,int idx,char ch,int dp[][]){
+        if(idx==s.length()) return 0;
+        if(dp[idx][ch]!=-1) return dp[idx][ch];
+        int pick=0;
+             if(Math.abs(ch-s.charAt(idx))<=k || ch=='#') pick=1+helper_function(s,k,idx+1,s.charAt(idx),dp); 
+             int notpick=helper_function(s,k,idx+1,ch,dp);
+       return dp[idx][ch]=Math.max(pick,notpick);
+        
+    }
     public int longestIdealString(String s, int k) {
-        int ans=0,n=s.length();
-        int dp[]=new int[26];
-        for(int i=0; i<n; i++){
-            int ch=s.charAt(i)-'a';
-            dp[ch]+=1;
-            for(int j=Math.max(0,ch-k); j<=Math.min(25,ch+k); j++){
-                if(j!=ch)
-                    dp[ch]=Math.max(dp[j]+1,dp[ch]);
-            }
-            ans=Math.max(ans,dp[ch]);
-        }
-        return ans;
+        int n=s.length();
+        int dp[][]=new int[n+1][150];
+        for(int sub_array[]:dp)
+         Arrays.fill(sub_array,-1);
+        return helper_function(s,k,0,'#',dp); // taken # as dummy char becuase in ascii table # is adjcent of z , so it will automatically cover by 150 dimension of dp
+        
     }
 }
