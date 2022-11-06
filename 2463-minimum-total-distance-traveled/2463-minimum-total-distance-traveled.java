02ed4bd6cc56bc25e85f68c4ca7e08f67a237ef9
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-11-07 01:29:12 +0530

    Time: 389 ms (33.33%), Space: 175 MB (33.33%) - LeetHub

--- 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.java
+++ 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.java
@@ -6,25 +6,23 @@ class Solution {
         Arrays.sort(factory, (o1, o2) -> (o1[0] - o2[0]));
         Collections.sort(robot);
         list = new ArrayList<>();
-        
+
         for (int arr[] : factory) {
             for (int i = 0; i < arr[1]; i++) list.add(arr[0]);
         }
-        dp=new Long[robot.size()+1][list.size()];
-        return helper(robot,0,0);
+        dp = new Long[robot.size() + 1][list.size()];
+        return helper(robot, 0, 0);
     }
-    
-    long helper(List<Integer> robot,int i,int j){
-        if(robot.size()==i) return 0;
-        if(list.size()==j) return (long)1e15;
-        
-        String key=i+","+j;
-        
-        if(dp[i][j]!=null) return dp[i][j];
-        
-        long take=Math.abs(robot.get(i)-list.get(j))+helper(robot,i+1,j+1);
-        long nottake=helper(robot,i,j+1);
-        return dp[i][j]=Math.min(take,nottake);
 
+    long helper(List<Integer> robot, int i, int j) {
+        if (robot.size() == i) return 0;
+        if (list.size() == j) return (long) 1e15;
+
+
+        if (dp[i][j] != null) return dp[i][j];
+
+        long take = Math.abs(robot.get(i) - list.get(j)) + helper(robot, i + 1, j + 1);
+        long nottake = helper(robot, i, j + 1);
+        return dp[i][j] = Math.min(take, nottake);
     }
 }
