3aef53e638748dc39d15a5b33acadc12c6d26d53
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-11-08 15:00:02 +0530

    Time: 415 ms (5.15%), Space: 43.9 MB (57.35%) - LeetHub

--- 1147-longest-chunked-palindrome-decomposition/1147-longest-chunked-palindrome-decomposition.java
+++ 1147-longest-chunked-palindrome-decomposition/1147-longest-chunked-palindrome-decomposition.java
@@ -4,7 +4,7 @@ class Solution {
     public int longestDecomposition(String text) {
         dp = new int[text.length()];
         Arrays.fill(dp, -1);
-        return (helper(text, 0))/2;
+        return (helper(text, 0)) / 2;
     }
 
     int helper(String text, int idx) {
@@ -14,14 +14,15 @@ class Solution {
             // System.out.println("HI");
             return dp[idx];
         }
-        String str1 = "", str2 = "";
+        StringBuilder str1 = new StringBuilder(), str2 = new StringBuilder();
         int max = 0;
-        for (int i = idx; i< n; i++) {
-            str1 += text.charAt(i);
-            str2 = text.charAt(n - i - 1) + str2;
-            if (str1.equals(str2)) {
-                int temp =0;
-                if(i == n - i - 1) temp = 1;
+        for (int i = idx; i < n; i++) {
+            str1.append(text.charAt(i));
+            str2.insert(0, text.charAt(n - i - 1));
+            // str2 = text.charAt(n - i - 1) + str2;
+            if (str1.toString().equals(str2.toString())) {
+                int temp = 0;
+                if (i == n - i - 1) temp = 1;
                 temp = 2 + helper(text, i + 1);
                 max = Math.max(max, temp);
             }
