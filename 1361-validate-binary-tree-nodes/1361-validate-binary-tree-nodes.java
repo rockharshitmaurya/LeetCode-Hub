fdb2172a14e2d2a159624c82a0496a0a6a35168e
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-11-10 23:21:50 +0530

    Time: 6 ms (85.12%), Space: 55.1 MB (55.71%) - LeetHub

--- 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.java
+++ 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.java
@@ -6,10 +6,12 @@ class Solution {
         return parent[x] = find(parent[x]);
     }
 
-    void union(int x, int y) {
+    boolean union(int x, int y) {
         int a = find(x);
         int b = find(y);
+        if (a == b) return true;
         parent[a] = b;
+        return false;
     }
 
     public boolean validateBinaryTreeNodes(int n, int[] left, int[] right) {
@@ -25,26 +27,25 @@ class Solution {
                 int a = find(i);
                 int b = find(left[i]);
                 ind[left[i]]++;
-                if (a == b) return false;
-                union(i, left[i]);
+                if (union(i, left[i])) return false;
             }
             if (right[i] != -1) {
                 int a = find(i);
                 int b = find(right[i]);
                 ind[right[i]]++;
-                if (a == b) return false;
-                union(i, right[i]);
+                if (union(i, right[i])) return false;
             }
         }
-        int count=0,root=-1;
-        for(int i=0; i<n; i++){
-            if(ind[i]==0){
+        int count = 0, root = -1;
+
+        for (int i = 0; i < n; i++) {
+            if (ind[i] == 0) {
                 count++;
-                root=i;
+                root = i;
             }
         }
-        
-        if(count!=1) return false;
+
+        if (count != 1) return false;
         return true;
     }
 }
