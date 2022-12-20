498f1de3070a42044f673f4a606381db9557b0cd
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2022-12-20 19:16:58 +0530

    Time: 1 ms (94.27%), Space: 44.8 MB (33.73%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -1,23 +1,21 @@
 class Solution {
+
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
         int n=rooms.size();
-        
-        
-        int ind[]=new int[n];
-        
-        Queue<Integer> q=new LinkedList<>();
-        q.offer(0);
-        
-        while(q.size()>0){
-            int node=q.poll();
-            ind[node]++;
-            
-            for(int child:rooms.get(node)){
-                if(ind[child]==0) q.offer(child);
-            }
+        int vis[]=new int[n];
+        helper(rooms,vis,0);
+        for(int key:vis){
+            if(key==0) return false;
         }
-        
-        for(int node:ind) if(node==0) return false;
         return true;
+    }
+    
+    void helper(List<List<Integer>> rooms,int vis[],int key){
+        if(vis[key]!=0) return;
+        vis[key]++;
+        
+        for(int keys:rooms.get(key)){
+            helper(rooms,vis,keys);
+        }
     }
 }
