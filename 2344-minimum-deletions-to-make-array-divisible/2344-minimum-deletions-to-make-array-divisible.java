a64a982f7716cfb41ce7353e1890e471ed8344fe
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2023-01-02 20:29:22 +0530

    Time: 12 ms (85.59%), Space: 58 MB (84.75%) - LeetHub

--- 2344-minimum-deletions-to-make-array-divisible/2344-minimum-deletions-to-make-array-divisible.java
+++ 2344-minimum-deletions-to-make-array-divisible/2344-minimum-deletions-to-make-array-divisible.java
@@ -1,45 +1,38 @@
 class Solution {
     public int minOperations(int[] nums, int[] numsDivide) {
-            int n_min=Integer.MAX_VALUE,d_min=Integer.MAX_VALUE;
-            PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
-            for(int num:nums){
-                n_min=Math.min(n_min,num);
-                pq.add(num);
-                if(n_min==1) return 0;
-            }
-            d_min=findGCD(numsDivide,numsDivide.length);
-            for(int num:numsDivide){
-                if(num%d_min!=0) return -1;
-            }
-            while(!pq.isEmpty()){
-                int num=pq.peek();
-                if(d_min%num!=0){
-                    pq.poll();
-                }else{
-                    break;
-                }
-            }
-            if(pq.isEmpty()) return -1;
-            return nums.length-pq.size();
+        Arrays.sort(nums);
+        int n=numsDivide.length;
+        int gcd=findGCD(numsDivide,n);
+        
+        int count=0;
+        for(int num:nums){
+            if(gcd%num==0) return count;
+            count++;
+        }
+        
+        return -1;
     }
      int gcd(int a, int b)
-	{
-		if (a == 0)
-			return b;
-		return gcd(b % a, a);
-	}
-	 int findGCD(int arr[], int n)
-	{
-		int result = arr[0];
-		for (int element: arr){
-			result = gcd(result, element);
-
-			if(result == 1)
-			{
-			return 1;
-			}
-		}
-
-		return result;
-	}
+    {
+        if (a == 0)
+            return b;
+        return gcd(b % a, a);
+    }
+ 
+    // Function to find gcd of array of
+    // numbers
+    int findGCD(int arr[], int n)
+    {
+        int result = arr[0];
+        for (int element: arr){
+            result = gcd(result, element);
+ 
+            if(result == 1)
+            {
+               return 1;
+            }
+        }
+ 
+        return result;
+    }
 }
