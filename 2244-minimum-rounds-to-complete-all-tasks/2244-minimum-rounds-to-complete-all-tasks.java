2bed8cec6be68831e70b51863609e4c9da2123b3
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2023-01-04 12:57:33 +0530

    Time: 95 ms (30.26%), Space: 104.5 MB (37.22%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.java
@@ -12,8 +12,6 @@ class Solution {
         for (int count : map.values()) {
             if(count==1) {
                 return -1;
-            }else if(count==2 || count==3){
-                ans++;
             }
             else if (count % 3 == 0) {
                 ans += (count / 3);
