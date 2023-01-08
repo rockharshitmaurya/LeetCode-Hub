e2016ffaf17525eada1573dcd39ab2308c804d40
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2023-01-08 16:45:19 +0530

    Time: 21 ms (89.62%), Space: 42.1 MB (85.36%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.java
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.java
@@ -1,27 +1,22 @@
 class Solution {
+
     public int maxPoints(int[][] points) {
-        int n=points.length;
-        if(n==1) return 1;
-        int ans=0;
-        for(int i=0; i<n; i++){
-            double x1=points[i][0],y1=points[i][1];
-            Map<Double,Integer> map=new HashMap<>();
-            for(int j=0; j<n; j++){ 
-                if(i==j) continue;
-                double x2=points[j][0],y2=points[j][1];
-                
-                 double dy=(y2-y1),dx=(x2-x1);
-                // System.out.println(dy+" "+dx);
-                 double slope=dy/dx;              
-                map.put(slope,map.getOrDefault(slope,1)+1);
-                ans=Math.max(ans,map.get(slope));
-                if(ans==6){
-                    System.out.println(map);
-                    System.out.println(((y2-y1)/(x2-x1)));
-                }
+        int n = points.length;
+        if (n == 1) return 1;
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            double x1 = points[i][0], y1 = points[i][1];
+            Map<Double, Integer> map = new HashMap<>();
+            for (int j = 0; j < n; j++) {
+                if (i == j) continue;
+                double x2 = points[j][0], y2 = points[j][1];
+                double dy = (y2 - y1), dx = (x2 - x1);
+                double slope = dy / dx;
+                map.put(slope, map.getOrDefault(slope, 1) + 1);
+                ans = Math.max(ans, map.get(slope));
             }
         }
-        
+
         return ans;
     }
 }
