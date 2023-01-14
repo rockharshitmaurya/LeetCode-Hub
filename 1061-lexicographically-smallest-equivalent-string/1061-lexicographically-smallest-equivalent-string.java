8a7460821b2d329ed86521531f33613586bbb917
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2023-01-14 15:04:30 +0530

    Time: 2 ms (88.07%), Space: 41.1 MB (78.41%) - LeetHub

---
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.java
@@ -0,0 +1,36 @@
+class Solution {
+    int parent[]=new int[26],map[]=new int[26];
+    int find(int x,int min) {
+        if (parent[x] == x) {
+            map[x]=Math.min(map[x],min);
+            return x;
+        }
+        return parent[x] = find(parent[x],Math.min(min,parent[x]));
+    }
+    
+    void union(int a,int b){
+        int x=find(a,a);
+        int y=find(b,b);
+        int low=Math.min(map[x],map[y]);
+        map[x]=map[y]=low;
+        parent[x]=y;
+    }
+    
+    public String smallestEquivalentString(String s1, String s2, String baseStr) {
+        for(int i=0; i<26; i++){
+           map[i]=parent[i]=i;
+        }
+
+        for(int i=0; i<s1.length(); i++){
+            union(s1.charAt(i)-'a',s2.charAt(i)-'a');
+        }
+        StringBuilder sb=new StringBuilder();
+        for(char ch:baseStr.toCharArray()){
+            sb.append((char)(map[find(ch-'a',ch-'a')]+'a'));
+        }
+        // System.out.println(Arrays.toString(map));
+
+        
+        return sb.toString();
+    }
+}
