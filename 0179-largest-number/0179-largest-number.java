5b8434ab15839a2632f17445cf22ec9a62b545f3
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2023-01-25 16:18:04 +0530

    Time: 8 ms (49.86%), Space: 43.7 MB (26.67%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -6,22 +6,9 @@ class Solution {
         for (int i = 0; i < n; i++) {
             str[i] = String.valueOf(nums[i]);
         }
-
-        Arrays.sort(
-            str,
-            (str1, str2) -> {
-                int n1 = str1.length(), n2 = str2.length();
-                for (int i = 0; i < Math.max(n1, n2); i++) {
-                    if (str1.charAt(i % n1) != str2.charAt(i % n2)) {
-                        int one = str1.charAt(i % n1) - '0';
-                        int two = str2.charAt(i % n2) - '0';
-                        return two - one;
-                    }
-                }
-
+        Arrays.sort(str,(str1, str2) -> {
                 String oneTwo = str1 + str2;
                 String twoOne = str2 + str1;
-
                 return twoOne.compareTo(oneTwo);
             }
         );
