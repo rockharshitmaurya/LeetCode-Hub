249ccc8c697a74f91f77c3cec8c27f5dc5bd2ebd
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2023-01-25 16:22:02 +0530

    Time: 8 ms (49.86%), Space: 44 MB (21.92%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -3,25 +3,14 @@ class Solution {
     public String largestNumber(int[] nums) {
         int n = nums.length;
         String str[] = new String[n];
-        for (int i = 0; i < n; i++) {
-            str[i] = String.valueOf(nums[i]);
-        }
-        Arrays.sort(str,(str1, str2) -> {
-                String oneTwo = str1 + str2;
-                String twoOne = str2 + str1;
-                return twoOne.compareTo(oneTwo);
-            }
-        );
+        for (int i = 0; i < n; i++) str[i] = String.valueOf(nums[i]);
+
+        Arrays.sort(str, (str1, str2) -> ((str2 + str1).compareTo(str1 + str2)));
         StringBuilder sb = new StringBuilder();
-        for (String s : str) {
-            sb.append(s);
-        }
+        for (String s : str) sb.append(s);
         int idx = -1;
-        for (int i = 0; i < sb.length(); i++) {
-            if (sb.charAt(i) != '0') {
-                idx = i;
-                break;
-            }
+        for (int i = 0; i < sb.length() && idx == -1; i++) {
+            if (sb.charAt(i) != '0') idx = i;
         }
         return idx == -1 ? "0" : sb.toString().substring(idx);
     }
