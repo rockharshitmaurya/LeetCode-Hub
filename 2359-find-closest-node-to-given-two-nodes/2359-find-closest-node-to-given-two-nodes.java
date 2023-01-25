99dbd90a9d3fb3e39a894dfb3136226f6f5eb531
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2023-01-25 12:48:52 +0530

    Time: 14 ms (90.42%), Space: 106.7 MB (29.94%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.java
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.java
@@ -1,25 +1,28 @@
 class Solution {
+
     public int closestMeetingNode(int[] edges, int node1, int node2) {
-        int n=edges.length;
-        int arr_1[]=new int[n]; Arrays.fill(arr_1,-1);
-        int arr_2[]=new int[n]; Arrays.fill(arr_2,-1);
-        dfs(edges,node1,0,arr_1);
-        dfs(edges,node2,0,arr_2);
-        int idx=-1,temp=Integer.MAX_VALUE;
-        for(int i=0; i<n; i++){
-            if(arr_1[i]!=-1 && arr_2[i]!=-1){
-                if(Math.max(arr_1[i],arr_2[i])<temp){
-                    temp=Math.max(arr_1[i],arr_2[i]);
-                    idx=i;
+        int n = edges.length;
+        int arr_1[] = new int[n];
+        Arrays.fill(arr_1, -1);
+        int arr_2[] = new int[n];
+        Arrays.fill(arr_2, -1);
+        dfs(edges, node1, 0, arr_1);
+        dfs(edges, node2, 0, arr_2);
+        int idx = -1, temp = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            if (arr_1[i] != -1 && arr_2[i] != -1) {
+                if (Math.max(arr_1[i], arr_2[i]) < temp) {
+                    temp = Math.max(arr_1[i], arr_2[i]);
+                    idx = i;
                 }
             }
         }
         return idx;
     }
-    void dfs(int edges[],int root,int len,int dis[]){
-        while(root!=-1 && dis[root]==-1){
-            dis[root]=len++;
-            root=edges[root];
+    void dfs(int edges[], int root, int len, int dis[]) {
+        while (root != -1 && dis[root] == -1) {
+            dis[root] = len++;
+            root = edges[root];
         }
     }
 }
