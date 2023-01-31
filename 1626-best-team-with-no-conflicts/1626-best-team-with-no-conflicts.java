4132cc403d304937f0c58e966038057709787117
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2023-01-31 17:26:37 +0530

    Time: 466 ms (6.12%), Space: 50.2 MB (32.65%) - LeetHub

---
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.java
@@ -0,0 +1,38 @@
+class Solution {
+    int dp[][];
+
+    public int bestTeamScore(int[] scores, int[] ages) {
+        int n = ages.length;
+        int pair[][] = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            pair[i] = new int[] { scores[i], ages[i] };
+        }
+        Arrays.sort(
+            pair,
+            (o1, o2) -> {
+                if (o1[1] == o2[1]) {
+                    return o1[0] - o2[0];
+                }
+                return o1[1] - o2[1];
+            }
+        );
+        dp = new int[n][n];
+        for (int arr[] : dp) Arrays.fill(arr, -1);
+        return helper(pair, 0, -1);
+    }
+
+    int helper(int arr[][], int idx, int last) {
+        if (idx == arr.length) return 0;
+
+        if (last != -1 && arr[last][0] > arr[idx][0]) return helper(arr, idx + 1, last);
+        if (last != -1 && dp[idx][last] != -1) return dp[idx][last];
+        int next = (last == -1 || arr[last][0] <= arr[idx][0]) ? idx : last;
+        int choose = arr[idx][0] + helper(arr, idx + 1, next);
+        int notchoose = helper(arr, idx + 1, last);
+        if (last != -1) {
+            dp[idx][last] = Math.max(choose, notchoose);
+            return dp[idx][last];
+        }
+        return Math.max(choose, notchoose);
+    }
+}
