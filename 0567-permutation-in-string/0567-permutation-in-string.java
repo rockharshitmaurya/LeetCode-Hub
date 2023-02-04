122596c0915b993e875b464b60367c09c51a9c06
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2023-02-04 13:06:24 +0530

    Time: 3 ms (99.34%), Space: 41.8 MB (98.65%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -12,13 +12,12 @@ class Solution {
         while (r < n2) {
             char ch = s2.charAt(r);
             hash[ch - 'a']++;
-            // if (count[ch - 'a'] == 0 || hash[ch - 'a'] > count[ch - 'a']) {
                 while (l <= r && count[ch - 'a'] == 0 || hash[ch - 'a'] > count[ch - 'a']) {
+                    //checking either the current char is not present in str1 or frequency of the current char is greater than str1
                     char ch2 = s2.charAt(l);
                     hash[ch2 - 'a']--;
                     l++;
                 }
-            // }
             if ((r - l + 1) == n1) return true;
             r++;
         }
