e0c7c80072ed97bb572b27e5e032771442ea26c7
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2023-02-05 13:31:45 +0530

    Time: 27 ms (100.00%), Space: 51.9 MB (100.00%) - LeetHub

---
+++ 2560-house-robber-iv/2560-house-robber-iv.java
@@ -0,0 +1,22 @@
+class Solution {
+
+    public int minCapability(int[] nums, int k) {
+        int left = 1, right = (int) 1e9, n = nums.length;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            int take = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] <= mid) {
+                    take++;
+                    i++;
+                }
+            }
+            if (take >= k) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
