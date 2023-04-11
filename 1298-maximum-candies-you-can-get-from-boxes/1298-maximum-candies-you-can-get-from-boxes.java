260a5a884ef92ee83ee8468a0b093ffa3a6c61f4
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2023-04-12 02:40:30 +0530

    Time: 18 ms (19.78%), Space: 55.7 MB (50.55%) - LeetHub

---
+++ 1298-maximum-candies-you-can-get-from-boxes/1298-maximum-candies-you-can-get-from-boxes.java
@@ -0,0 +1,36 @@
+class Solution {
+
+    public int maxCandies(int[] status, int[] candies, int[][] keys, int[][] containedBoxes, int[] initialBoxes) {
+        Queue<Integer> q = new LinkedList<>();
+        HashSet<Integer> key = new HashSet<>();
+        HashSet<Integer> vis = new HashSet<>();
+        int ans = 0;
+        for (int box : initialBoxes) {
+            q.offer(box);
+            for (int xkey : keys[box]) {
+                key.add(xkey);
+            }
+        }
+
+        while (!q.isEmpty()) {
+            int box = q.poll();
+
+            if (vis.contains(box)) continue;
+            vis.add(box);
+
+            if (status[box] == 1 || key.contains(box)) {
+                ans += candies[box];
+                
+                for (int boxx : containedBoxes[box]) {
+                    q.offer(boxx);
+                    for (int xkey : keys[boxx]) {
+                        key.add(xkey);
+                    }
+                }
+            }
+        }
+
+        System.out.println(q);
+        return ans;
+    }
+}
