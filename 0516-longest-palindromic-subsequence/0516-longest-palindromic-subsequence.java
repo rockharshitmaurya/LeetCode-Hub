1e7ede8506fbcfa772b77713791a04687847a40d
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2023-04-14 12:38:36 +0530

    Time: 99 ms (24.22%), Space: 51.9 MB (51.59%) - LeetHub

---
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int longestPalindromeSubseq(String text1) {
+        String text2=new StringBuilder(text1).reverse().toString(); // question boild down to find longest comman subsequnece #striverDP26
+        int n=text1.length(),m=text2.length();
+         int dp[][]=new int[n+1][m+1];
+        for(int idx1=1; idx1<=n; idx1++){
+            for(int idx2=1; idx2<=m; idx2++){
+                        if(text1.charAt(idx1-1)==text2.charAt(idx2-1)) dp[idx1][idx2]=1+dp[idx1-1][idx2-1];
+                        else{ 
+                            int left=dp[idx1-1][idx2];
+                            int right=dp[idx1][idx2-1];
+                            dp[idx1][idx2]=Math.max(left,right);
+                        }
+            }
+        }
+        return dp[n][m];
+    }
+}
