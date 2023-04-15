5b7dcf2d890e939b8dbd15db5212f626cb22ede1
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2023-04-15 14:53:33 +0530

    Time: 228 ms (48.76%), Space: 46.1 MB (40.12%) - LeetHub

---
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.java
@@ -0,0 +1,26 @@
+class Solution {
+    int dp[][];
+
+    public int maxValueOfCoins(List<List<Integer>> piles, int k) {
+        dp = new int[piles.size()][k+1];
+        for (int arr[] : dp) {
+            Arrays.fill(arr, -1);
+        }
+        return helper(piles, k, 0);
+    }
+
+    int helper(List<List<Integer>> piles, int k, int idx) {
+        if (k == 0 || idx == piles.size()) return 0;
+
+        if (dp[idx][k] != -1) return dp[idx][k];
+        int sum = 0;
+        int max = helper(piles, k, idx + 1);
+        for (int i = 0; i < piles.get(idx).size(); i++) {
+            if (k - (i + 1) < 0) continue;
+            sum += piles.get(idx).get(i);
+            max = Math.max(max, sum + helper(piles, k - (i + 1), idx + 1));
+        }
+
+        return dp[idx][k] = max;
+    }
+}
