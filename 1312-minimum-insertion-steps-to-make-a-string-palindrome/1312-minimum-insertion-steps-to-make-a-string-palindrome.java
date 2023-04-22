c6a0c70086cb473327e752297b965af63723a700
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2023-04-22 13:26:06 +0530

    Time: 58 ms (6.72%), Space: 43.8 MB (58.23%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,22 +1,25 @@
 class Solution {
+    int dp[][];
+
     public int minInsertions(String s) {
-       return s.length()-longestPalindromeSubseq(s);
-        //Ans should be len of the String -minus longest comman palindromic subsqeunce
+        String text2 = new StringBuilder(s).reverse().toString();
+        dp = new int[s.length()][s.length()];
+        Arrays.stream(dp).forEach(a -> Arrays.fill(a, -1));
+
+        return s.length() - helper(s, text2, 0, 0);
     }
-    public int longestPalindromeSubseq(String text1) {
-        String text2=new StringBuilder(text1).reverse().toString(); // question boild down to find longest comman subsequnece #striverDP26
-        int n=text1.length(),m=text2.length();
-         int dp[][]=new int[n+1][m+1];
-        for(int idx1=1; idx1<=n; idx1++){
-            for(int idx2=1; idx2<=m; idx2++){
-                        if(text1.charAt(idx1-1)==text2.charAt(idx2-1)) dp[idx1][idx2]=1+dp[idx1-1][idx2-1];
-                        else{ 
-                            int left=dp[idx1-1][idx2];
-                            int right=dp[idx1][idx2-1];
-                            dp[idx1][idx2]=Math.max(left,right);
-                        }
-            }
-        }
-        return dp[n][m];
+
+    int helper(String str1, String str2, int l, int r) {
+        if (l == str1.length() || r == str2.length()) return 0;
+
+        if(dp[l][r]!=-1) return dp[l][r];
+        if (str1.charAt(l) == str2.charAt(r)) return 1 + helper(str1, str2, l + 1, r + 1);
+
+        int left = helper(str1, str2, l + 1, r);
+        int right = helper(str1, str2, l, r + 1);
+
+        return dp[l][r]=Math.max(left, right);
     }
 }
+// zzazz
+// zzazz
