2ca6ba44b7793d38325bd53b96aa38bd1210d3df
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2023-04-25 19:05:20 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (60.33%) - LeetHub

--- 0765-couples-holding-hands/0765-couples-holding-hands.java
+++ 0765-couples-holding-hands/0765-couples-holding-hands.java
@@ -17,46 +17,41 @@ class Solution {
                 continue;
             }
 
-            if (row[i] % 2 == 0) {
-                
-                int next_val=row[i+1];
-                int target_val=row[i]+1;
-                
-                
-                int next_idx = arr[next_val];
-                int target_idx = arr[target_val];
-                                     
-                
-                // System.out.println(target_idx+" "+next_idx+" "+i);
+            boolean isEven = row[i] % 2 == 0;
 
-                int temp = row[next_idx];
-                row[next_idx] = row[target_idx];
-                row[target_idx] = temp;
+            // if (row[i] % 2 == 0) {
+            int next_val = row[i + 1];
+            int target_val = row[i] + (isEven ? 1 : -1);
 
-                arr[next_val] = target_idx;
-                arr[target_val] = next_idx;
-                                     
-                // System.out.println(Arrays.toString(row));
-            } else if (row[i] % 2 == 1) {
-                int next_val=row[i+1];
-                int target_val=row[i]-1;
-                
-                
-                int next_idx = arr[next_val];
-                int target_idx = arr[target_val];
-                                     
-                
-                // System.out.println(target_idx+" "+next_idx+" "+i);
+            int next_idx = arr[next_val];
+            int target_idx = arr[target_val];
 
-                int temp = row[next_idx];
-                row[next_idx] = row[target_idx];
-                row[target_idx] = temp;
+            // System.out.println(target_idx+" "+next_idx+" "+i);
 
-                arr[next_val] = target_idx;
-                arr[target_val] = next_idx;
-                                     
-                // System.out.println(Arrays.toString(row));
-            }
+            int temp = row[next_idx];
+            row[next_idx] = row[target_idx];
+            row[target_idx] = temp;
+
+            arr[next_val] = target_idx;
+            arr[target_val] = next_idx;
+            // System.out.println(Arrays.toString(row));
+            //             } else if (row[i] % 2 == 1) {
+            //                 int next_val = row[i + 1];
+            //                 int target_val = row[i] - 1;
+
+            //                 int next_idx = arr[next_val];
+            //                 int target_idx = arr[target_val];
+
+            //                 // System.out.println(target_idx+" "+next_idx+" "+i);
+
+            //                 int temp = row[next_idx];
+            //                 row[next_idx] = row[target_idx];
+            //                 row[target_idx] = temp;
+
+            //                 arr[next_val] = target_idx;
+            //                 arr[target_val] = next_idx;
+            //                 // System.out.println(Arrays.toString(row));
+            //             }
 
             ans++;
         }
@@ -64,6 +59,5 @@ class Solution {
         return ans;
     }
 }
-
 // [0,1,7,6,2,3,4,5]
 // [0,3,7,4,2,5,6,1]
