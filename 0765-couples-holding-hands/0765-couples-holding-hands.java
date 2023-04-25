2632b942c62d23f469f4a32331c31ba7645b9014
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2023-04-25 19:06:04 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (60.33%) - LeetHub

--- 0765-couples-holding-hands/0765-couples-holding-hands.java
+++ 0765-couples-holding-hands/0765-couples-holding-hands.java
@@ -19,14 +19,11 @@ class Solution {
 
             boolean isEven = row[i] % 2 == 0;
 
-            // if (row[i] % 2 == 0) {
             int next_val = row[i + 1];
             int target_val = row[i] + (isEven ? 1 : -1);
 
             int next_idx = arr[next_val];
             int target_idx = arr[target_val];
-
-            // System.out.println(target_idx+" "+next_idx+" "+i);
 
             int temp = row[next_idx];
             row[next_idx] = row[target_idx];
@@ -34,30 +31,10 @@ class Solution {
 
             arr[next_val] = target_idx;
             arr[target_val] = next_idx;
-            // System.out.println(Arrays.toString(row));
-            //             } else if (row[i] % 2 == 1) {
-            //                 int next_val = row[i + 1];
-            //                 int target_val = row[i] - 1;
-
-            //                 int next_idx = arr[next_val];
-            //                 int target_idx = arr[target_val];
-
-            //                 // System.out.println(target_idx+" "+next_idx+" "+i);
-
-            //                 int temp = row[next_idx];
-            //                 row[next_idx] = row[target_idx];
-            //                 row[target_idx] = temp;
-
-            //                 arr[next_val] = target_idx;
-            //                 arr[target_val] = next_idx;
-            //                 // System.out.println(Arrays.toString(row));
-            //             }
 
             ans++;
         }
-        // System.out.println(Arrays.toString(row));
         return ans;
     }
 }
-// [0,1,7,6,2,3,4,5]
-// [0,3,7,4,2,5,6,1]
+
