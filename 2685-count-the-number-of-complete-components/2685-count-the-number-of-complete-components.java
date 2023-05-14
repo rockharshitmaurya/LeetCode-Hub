98ae29afc0ca8bc0a8ea49705152a2572bb4d329
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2023-05-14 13:37:43 +0530

    Time: 29 ms (16.67%), Space: 43.1 MB (50.00%) - LeetHub

--- 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.java
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.java
@@ -1,65 +1,64 @@
 class Solution {
-    
     int parent[];
-    void union(int u,int v){
-        int a=find(u);
-        int b=find(v);
-        parent[a]=b;
+
+    void union(int u, int v) {
+        int a = find(u);
+        int b = find(v);
+        parent[a] = b;
     }
-    int find(int x){
-        if(parent[x]==x) return x;
-        return parent[x]=find(parent[x]);
+
+    int find(int x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
     }
-    HashMap<Integer,Integer> map=new HashMap<>();
-    
+
+    HashMap<Integer, Integer> map = new HashMap<>();
+
     public int countCompleteComponents(int n, int[][] edges) {
-        parent=new int[n];
-        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
-        for(int i=0; i<n; i++){
-            parent[i]=i;
+        parent = new int[n];
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
             adj.add(new ArrayList<>());
         }
-        
-        for(int arr[]:edges){
-            union(arr[0],arr[1]);
+
+        for (int arr[] : edges) {
+            union(arr[0], arr[1]);
             adj.get(arr[0]).add(arr[1]);
             adj.get(arr[1]).add(arr[0]);
         }
-        int ans=0;
-        boolean vis[]=new boolean[n];
+        int ans = 0;
 
-        for(int i=0; i<n; i++){
-            if(parent[i]==i){
-                 map=new HashMap<>();
-                 vis=new boolean[n];
-                 helper(adj,i,vis);
-                 if(calc()) ans++;
-                 // System.out.println(map);
+        for (int i = 0; i < n; i++) {
+            if (parent[i] == i) {
+                map = new HashMap<>();
+                helper(adj, i);
+                if (calc()) ans++;
+                // System.out.println(map);
             }
         }
-        
+
         return ans;
     }
-    
-    boolean calc(){
-        int size=map.size();
-         
-        for(int val:map.values()){
-            if(val!=size-1) return false;
+
+    boolean calc() {
+        int size = map.size();
+
+        for (int val : map.values()) {
+            if (val != size - 1) return false;
         }
-        
+
         return true;
     }
-    
-     void helper(ArrayList<ArrayList<Integer>> adj,int root,boolean vis[]){
-        if(map.containsKey(root)) return;
-         
-         vis[root]=true;
-         map.put(root,0);
-         
-         for(int child:adj.get(root)){
-             map.put(root,map.get(root)+1);
-             helper(adj,child,vis);
-         }
+
+    void helper(ArrayList<ArrayList<Integer>> adj, int root) {
+        if (map.containsKey(root)) return;
+
+        map.put(root, 0);
+
+        for (int child : adj.get(root)) {
+            map.put(root, map.get(root) + 1);
+            helper(adj, child);
+        }
     }
 }
