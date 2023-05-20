44c912dcd3e13748b5a5b356ad1eb83cb35b6cc6
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2023-05-20 11:43:03 +0530

    Time: 1 ms (90.67%), Space: 41.6 MB (9.41%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.java
+++ 0399-evaluate-division/0399-evaluate-division.java
@@ -1,28 +1,30 @@
 class Solution {
+
     public double[] calcEquation(List<List<String>> eq, double[] val, List<List<String>> qu) {
-        Map<String,Map<String,Double>> map=new HashMap<>();
-        for(int i=0; i<eq.size(); i++){
-            map.putIfAbsent(eq.get(i).get(0),new HashMap<String,Double>());
-            map.putIfAbsent(eq.get(i).get(1),new HashMap<String,Double>());
-            map.get(eq.get(i).get(0)).put(eq.get(i).get(1),val[i]);
-            map.get(eq.get(i).get(1)).put(eq.get(i).get(0),1.0/val[i]);
+        Map<String, Map<String, Double>> map = new HashMap<>();
+        for (int i = 0; i < eq.size(); i++) {
+            map.putIfAbsent(eq.get(i).get(0), new HashMap<String, Double>());
+            map.putIfAbsent(eq.get(i).get(1), new HashMap<String, Double>());
+            map.get(eq.get(i).get(0)).put(eq.get(i).get(1), val[i]);
+            map.get(eq.get(i).get(1)).put(eq.get(i).get(0), 1.0 / val[i]);
         }
-        double[] ans=new double[qu.size()];
-        for(int i=0; i<qu.size(); i++){
-           ans[i]=dfs(qu.get(i).get(0),qu.get(i).get(1),1,map,new HashSet<>()); 
+        double[] ans = new double[qu.size()];
+        for (int i = 0; i < qu.size(); i++) {
+            ans[i] = dfs(qu.get(i).get(0), qu.get(i).get(1), 1, map, new HashSet<>());
         }
-       return ans; 
+        return ans;
     }
+
     double dfs(String s, String t, double r, Map<String, Map<String, Double>> map, Set<String> seen) {
-        if(!map.containsKey(s)) return -1;
-        if(!seen.add(s)) return -1;
-        if(s.equals(t)) return r;
-        Map<String, Double> next=map.get(s);
-        for(String c:next.keySet()){
-            double ans=dfs(c,t,r*next.get(c),map,seen);
-            if(ans!=-1) return ans;
+        if (!map.containsKey(s)) return -1;
+        seen.add(s);
+        if (s.equals(t)) return r;
+        Map<String, Double> next = map.get(s);
+        for (String c : next.keySet()) {
+            if(seen.contains(c)) continue;
+            double ans = dfs(c, t, r * next.get(c), map, seen);
+            if (ans != -1) return ans;
         }
         return -1;
     }
-    
 }
