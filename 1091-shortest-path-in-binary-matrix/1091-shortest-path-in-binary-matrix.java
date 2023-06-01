9f1e54d8a02cd820003b423f4468a53d32589df2
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2023-06-01 15:59:35 +0530

    Time: 19 ms (43.72%), Space: 45 MB (16.70%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -4,7 +4,7 @@ class Solution {
         int n=grid.length,m=grid[0].length;
         if(grid[0][0]==1 || grid[n-1][m-1]==1) return -1;
         Queue<int[]> q=new LinkedList<>();
-        q.add(new int[]{0,0,0});
+        q.add(new int[]{0,0,0}); //arr[0]->x ,arr[1]->y,arr[2]=cost
         int dis[][]=new int[n][m];
         int dir[][]={{0,1},{1,0},{-1,0},{0,-1},{-1,-1},{1,1},{1,-1},{-1,1}};
         for(int arr[]:dis) Arrays.fill(arr,INF);
