177e5e97f42b6b8382c678af120271755e9f9d46
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2023-06-07 17:46:22 +0530

    Time: 1811 ms (5.56%), Space: 39.7 MB (72.22%) - LeetHub

---
+++ 1012-numbers-with-repeated-digits/1012-numbers-with-repeated-digits.java
@@ -0,0 +1,33 @@
+class Solution {
+
+    public int numDupDigitsAtMostN(int n) {
+        if(n<=10) return 0;
+        return (n-countNumbersWithUniqueDigits(n))+1;
+    }
+
+    int map[] = new int[10];
+
+    public int countNumbersWithUniqueDigits(int n) {
+        if (n == 0) return 1; else if (n == 1) return 10;
+        return helper(n + "", 0, 1, 0);
+    }
+
+    int helper(String num, int idx, int limit, int tsum) {
+        if (num.length() == idx) {
+            return 1;
+        }
+
+        int end = (limit == 1) ? (num.charAt(idx) - '0') : 9;
+        int ans = 0;
+
+        for (int dig = 0; dig <= end; dig++) {
+            int new_limit = (limit == 1 && dig == end) ? 1 : 0;
+            if (map[dig] > 0) continue;
+            if (tsum + dig > 0) map[dig]++;
+            ans = ans + helper(num, idx + 1, new_limit, tsum + dig);
+            if (tsum + dig > 0) map[dig]--;
+        }
+
+        return ans;
+    }
+}
