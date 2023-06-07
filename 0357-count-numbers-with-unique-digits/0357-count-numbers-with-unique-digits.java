ef7e8cdded500b69c8c6ef404c9c6aae163771b2
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2023-06-07 17:37:17 +0530

    Time: 278 ms (5.14%), Space: 43.4 MB (5.61%) - LeetHub

---
+++ 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.java
@@ -0,0 +1,33 @@
+class Solution {
+    int map[] = new int[10];
+
+    public int countNumbersWithUniqueDigits(int n) {
+        if(n==0) return 1;
+        else if(n==1) return 10;
+        int num=(int)Math.pow(10, n);
+        return helper(num+ "", 0, 1, 0,0,"->");
+    }
+
+    int helper(String num, int idx, int limit, int sum,int tsum,String str) {
+        
+        if (num.length() == idx) {
+            // System.out.println(str);
+            return 1;
+        }
+
+        
+        int end = (limit == 1) ? (num.charAt(idx) - '0') : 9;
+
+        int ans = 0;
+        for (int dig = 0; dig <= end; dig++) {
+            int new_limit = (limit == 1 && dig == end) ? 1 : 0;
+            if(map[dig]>0) continue;
+            if(tsum+dig>0) map[dig]++;
+            int to_add = (map[dig] == 1 ? 1 : 0);
+            ans = ans + helper(num, idx + 1, new_limit, sum + to_add,tsum+dig,str+dig);
+            if(tsum+dig>0) map[dig]--;
+        }
+        
+        return ans;
+    }
+}
