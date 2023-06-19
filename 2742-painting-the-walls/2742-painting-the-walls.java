622f14e15119743f0c01975515316673db5c9fca
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2023-06-19 05:47:40 +0530

    Time: 102 ms (20.00%), Space: 48.2 MB (60.00%) - LeetHub

--- 2742-painting-the-walls/2742-painting-the-walls.java
+++ 2742-painting-the-walls/2742-painting-the-walls.java
@@ -1,13 +1,14 @@
 class Solution {
     int inf = (int) 1e9 + 7;
-    int[][][] dp;
+    int[][] dp;
+    int offset;
 
     public int paintWalls(int[] cost, int[] time) {
         int n = cost.length;
-        dp = new int[n + 1][n + 1][2];
-        for (int[][] arr : dp)
-            for (int[] arr2 : arr)
-                Arrays.fill(arr2, -1);
+        offset = n;
+        dp = new int[n + 1][2 * n + 1];
+        for (int[] arr : dp)
+            Arrays.fill(arr, -1);
         return helper(cost, time, 0, 0);
     }
 
@@ -17,15 +18,14 @@ class Solution {
         }
         if (cur_time >= time.length) return 0;
 
-        int flip = (cur_time < 0) ? 0 : 1;
-        if (dp[idx][Math.abs(cur_time)][flip] != -1) {
-            return dp[idx][Math.abs(cur_time)][flip];
+        if (dp[idx][cur_time + offset] != -1) {
+            return dp[idx][cur_time + offset];
         }
 
         int pick = helper(cost, time, idx + 1, cur_time - 1);
         int notpick = cost[idx] + helper(cost, time, idx + 1, cur_time + time[idx]);
 
-        dp[idx][Math.abs(cur_time)][flip] = Math.min(pick, notpick);
+        dp[idx][cur_time + offset] = Math.min(pick, notpick);
         return Math.min(pick, notpick);
     }
 }
