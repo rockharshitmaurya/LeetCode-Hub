9abf00d958e6533c8a5421e19c153ce96d91895c
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2023-06-19 05:45:43 +0530

    Time: 271 ms (20.00%), Space: 57.5 MB (20.00%) - LeetHub

--- 2742-painting-the-walls/2742-painting-the-walls.java
+++ 2742-painting-the-walls/2742-painting-the-walls.java
@@ -1,28 +1,31 @@
 class Solution {
     int inf = (int) 1e9 + 7;
-    int dp[][][];
+    int[][][] dp;
 
     public int paintWalls(int[] cost, int[] time) {
         int n = cost.length;
         dp = new int[n + 1][n + 1][2];
-        // map = new int[n + 1];
-        for (int arr[][] : dp) for (int arr2[] : arr) Arrays.fill(arr2, -1);
+        for (int[][] arr : dp)
+            for (int[] arr2 : arr)
+                Arrays.fill(arr2, -1);
         return helper(cost, time, 0, 0);
     }
 
-    int helper(int cost[], int time[], int idx, int cur_time) {
+    int helper(int[] cost, int[] time, int idx, int cur_time) {
         if (idx == time.length) {
-            if (cur_time >= 0) return 0; else return inf;
+            return (cur_time >= 0) ? 0 : inf;
         }
         if (cur_time >= time.length) return 0;
-        int pick = inf, notpick = inf;
+
         int flip = (cur_time < 0) ? 0 : 1;
-        if (dp[idx][cur_time < 0 ? cur_time * -1 : cur_time][flip] != -1) return dp[idx][cur_time < 0 ? cur_time * -1 : cur_time][flip];
+        if (dp[idx][Math.abs(cur_time)][flip] != -1) {
+            return dp[idx][Math.abs(cur_time)][flip];
+        }
 
-        pick = helper(cost, time, idx + 1, cur_time - 1);
-        notpick = cost[idx] + helper(cost, time, idx + 1, cur_time + time[idx]);
+        int pick = helper(cost, time, idx + 1, cur_time - 1);
+        int notpick = cost[idx] + helper(cost, time, idx + 1, cur_time + time[idx]);
 
-        dp[idx][cur_time < 0 ? cur_time * -1 : cur_time][flip] = Math.min(pick, notpick);
+        dp[idx][Math.abs(cur_time)][flip] = Math.min(pick, notpick);
         return Math.min(pick, notpick);
     }
 }
