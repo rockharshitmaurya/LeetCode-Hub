ffd3b76cc33a4ceb5bf02ac9410197fadff81367
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2023-07-08 00:05:30 +0530

    Time: 10 ms (97.81%), Space: 43.5 MB (75.94%) - LeetHub

---
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxConsecutiveAnswers(String s, int k) {
+        int res = 0, maxf = 0, count[] = new int[128];
+        for (int i = 0; i < s.length(); ++i) {
+            maxf = Math.max(maxf, ++count[s.charAt(i)]);
+            if (res - maxf < k)
+                res++;
+            else
+                count[s.charAt(i - res)]--;
+        }
+        return res;
+
+    }
+}
