4d4683e91df7cf9f2f994424f9c018289a56456d
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2023-08-15 14:09:48 +0530

    Time: 5 ms (53.97%), Space: 44.6 MB (92.07%) - LeetHub

---
+++ 0690-employee-importance/0690-employee-importance.java
@@ -0,0 +1,32 @@
+/*
+// Definition for Employee.
+class Employee {
+    public int id;
+    public int importance;
+    public List<Integer> subordinates;
+};
+*/
+
+class Solution {
+    Map<Integer, Integer> map = new HashMap<>();
+    HashSet<Integer> set = new HashSet<>();
+
+    public int getImportance(List<Employee> employees, int id) {
+        for (int i = 0; i < employees.size(); i++) {
+            map.put(employees.get(i).id, i);
+        }
+        // System.out.println(map);
+        return helper(id,employees);
+    }
+
+    int helper(int root, List<Employee> employees) {
+        if (set.contains(root)) return 0;
+        set.add(root);
+        int sum = employees.get(map.get(root)).importance;
+        for (int child : employees.get(map.get(root)).subordinates) {
+            sum += helper(child, employees);
+        }
+
+        return sum;
+    }
+}
