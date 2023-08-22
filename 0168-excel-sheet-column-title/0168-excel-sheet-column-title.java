e219fd5639ad0c95f3cc6fcbd77f61eb63916459
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2023-08-22 11:51:44 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (29.14%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String convertToTitle(int n) {
+    StringBuffer sb = new StringBuffer();
+    while(n>0){
+        char tmp = (char)((n%26==0?26:n%26)+64);
+            sb.append(tmp);
+            n =(n%26==0? n/26-1: n/26);
+        }
+        return sb.reverse().toString();
+    }
+}
+// 26*0+25-->ans*26+2
