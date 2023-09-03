fe11451c4a9fdbaf87dd6e3d8da5a2dfbc48dc64
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2023-09-03 09:44:53 +0530

    Time: 132 ms (25.00%), Space: 69.4 MB (50.00%) - LeetHub

--- 2842-count-k-subsequences-of-a-string-with-maximum-beauty/2842-count-k-subsequences-of-a-string-with-maximum-beauty.java
+++ 2842-count-k-subsequences-of-a-string-with-maximum-beauty/2842-count-k-subsequences-of-a-string-with-maximum-beauty.java
@@ -1,58 +1,32 @@
 class Solution {
     int MOD = (int) 1e9 + 7;
-    int map[];
-    Map<String, Integer> dp;
+    int[] map;
 
     public int countKSubsequencesWithMaxBeauty(String s, int k) {
         map = new int[26];
         for (char ch : s.toCharArray()) {
             map[ch - 'a']++;
         }
-        int count = 0;
 
-        for (int num : map) {
-            if (num > 0) count++;
-        }
-
+        int count = (int) Arrays.stream(map).filter(x -> x > 0).count();
         if (k > count) return 0;
 
         Arrays.sort(map);
-
-        int k_copy = k;
-
         int max = Arrays.stream(map).skip(26 - k).sum();
 
-
-        dp = new HashMap<>();
-
-        return helper(0, k, max);
+        return helper(0, k, max, new HashMap<>());
     }
 
-    int helper(int idx, int k, int sum) {
+    int helper(int idx, int k, int sum, Map<String, Integer> dp) {
         if (k == 0 && sum == 0) return 1;
-
-        if (sum <= 0 || k <= 0) return 0;
-
-        if (idx == 26) return 0;
-
+        if (sum <= 0 || k <= 0 || idx == 26) return 0;
         String key = idx + "," + sum + "," + k;
-
         if (dp.containsKey(key)) return dp.get(key);
 
-        long pick = (map[idx] * 1L * helper(idx + 1, k - 1, sum - map[idx])) % MOD;
-        long notpick = helper(idx + 1, k, sum);
+        long pick = (map[idx] * 1L * helper(idx + 1, k - 1, sum - map[idx], dp)) % MOD;
+        long notpick = helper(idx + 1, k, sum, dp);
 
         dp.put(key, (int) (pick + notpick) % MOD);
-
         return dp.get(key);
     }
-        int setBit(int number, int i) {
-            int mask = 1 << i;
-            return number | mask;
-        }
-
-        boolean isBitSet(int number, int i) {
-            int mask = 1 << i;
-            return (number & mask) != 0;
-        }
 }
