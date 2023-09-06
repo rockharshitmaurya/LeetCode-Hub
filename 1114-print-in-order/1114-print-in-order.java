ab02aa82e7949b3ad383156a175bdae4a833a0bc
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2023-09-07 02:09:24 +0530

    Time: 13 ms (30.38%), Space: 41.7 MB (39.87%) - LeetHub

--- 1114-print-in-order/1114-print-in-order.java
+++ 1114-print-in-order/1114-print-in-order.java
@@ -1,39 +1,39 @@
 class Foo {
     int val;
-    // Object lock;
+    Object lock;
 
     public Foo() {
         val = 0;
-        // lock = new Object();
+        lock = new Object();
     }
 
     public void first(Runnable printFirst) throws InterruptedException {
         // printFirst.run() outputs "first". Do not change or remove this line.
-        synchronized (this) {
+        synchronized (lock) {
     
         printFirst.run();
         val = 1;
-        notifyAll();
+        lock.notifyAll();
         }
     }
 
     public void second(Runnable printSecond) throws InterruptedException {
-        synchronized (this) {
+        synchronized (lock) {
             while (val != 1) {
-                wait();
+                lock.wait();
             }
             printSecond.run();
             val = 2;
-            notifyAll();
+            lock.notifyAll();
             // printSecond.run() outputs "second". Do not change or remove this line.
             
         }
     }
 
     public void third(Runnable printThird) throws InterruptedException {
-        synchronized (this) {
+        synchronized (lock) {
             while (val != 2) {
-                wait();
+                lock.wait();
             }
             // val=1;
             // lock.notifyAll();
