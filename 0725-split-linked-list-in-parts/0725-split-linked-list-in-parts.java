a3b8905869ff03d91731541a7ef01bb279832fd8
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2023-09-06 15:06:28 +0530

    Time: 0 ms (100.00%), Space: 43.5 MB (74.14%) - LeetHub

--- 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.java
+++ 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.java
@@ -13,30 +13,16 @@ class Solution {
     public ListNode[] splitListToParts(ListNode head, int k) {
         ListNode[] ans = new ListNode[k];
         int len = 0;
-        ListNode temp = head;
-        while (temp != null) {
+        for (ListNode temp = head; temp != null; temp = temp.next) {
             len++;
-            temp = temp.next;
         }
-
-        // if (len <= k) {
-        //     for (int i = 0; i < len; i++) {
-        //         ans[i] = head;
-        //         head = head.next;
-        //         ans[i].next = null;
-        //     }
-        // } else {
-            int rem = len % k;
-            helper(head, 0, (len / k), ans, rem);
-        // }
-
-        return ans;
+        return helper(head, 0, (len / k), ans, (len % k));
     }
 
-    void helper(ListNode head, int idx, int len, ListNode[] ans, int rem) {
-        if (head == null) return;
+    ListNode[] helper(ListNode head, int idx, int len, ListNode[] ans, int rem) {
+        if (head == null) return ans;
         ans[idx] = head;
-
+        
         ListNode prev = null;
         int toadd = rem > 0 ? 1 : 0;
         for (int i = 1; i <= (len + toadd) && head != null; i++) {
@@ -44,8 +30,6 @@ class Solution {
             head = head.next;
         }
         prev.next = null;
-        if (head != null) {
-            helper(head, idx + 1, len, ans, Math.max(0, rem - 1));
-        }
+        return helper(head, idx + 1, len, ans, Math.max(0, rem - 1));
     }
 }
