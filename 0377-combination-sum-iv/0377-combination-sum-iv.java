a4b66e4fa1cdcbfe51a60e57b0a82104b90c8506
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2023-09-09 14:56:31 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (91.60%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -0,0 +1,22 @@
+class Solution {
+    int dp[];
+
+    public int combinationSum4(int[] nums, int target) {
+        dp = new int[target + 1];
+        Arrays.fill(dp, -1);
+        return helper(0, nums, target);
+    }
+
+    int helper(int idx, int nums[], int target) {
+        if (target < 0) return 0; else if (target == 0) return 1;
+
+        if (dp[target] != -1) return dp[target];
+
+        int add = 0;
+        for (int i = idx; i < nums.length; i++) {
+            add += helper(0, nums, target - nums[i]);
+        }
+
+        return dp[target] = add;
+    }
+}
