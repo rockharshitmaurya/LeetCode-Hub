cd81db278609d1c5026146c4f223c2a2e1d31168
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2023-09-13 22:04:45 +0530

    Time: 30 ms (14.02%), Space: 44.2 MB (24.98%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,28 +1,33 @@
 class Solution {
+
     public int candy(int[] ratings) {
-        int n=ratings.length;
-        int candies[]=new int[n];
-        Arrays.fill(candies,1);
-        
-        for(int i=1; i<n; i++){
-            if(ratings[i]>ratings[i-1]){
-                candies[i]+=candies[i-1];
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> (o1[0] - o2[0]));
+        int n = ratings.length;
+        for (int i = 0; i < n; i++) {
+            pq.offer(new int[] { ratings[i], i });
+        }
+        int sum = 0;
+        int candies[] = new int[n];
+        Arrays.fill(candies, 1);
+        while (!pq.isEmpty()) {
+            int arr[] = pq.poll();
+            int val = arr[0];
+            int idx = arr[1];
+            if (idx + 1 < n && ratings[idx] > ratings[idx + 1]) {
+                candies[idx] = Math.max(candies[idx], candies[idx + 1] + 1);
+            }
+
+            if (idx - 1 > -1 && ratings[idx] > ratings[idx - 1]) {
+                candies[idx] = Math.max(candies[idx], candies[idx - 1] + 1);
             }
         }
-        
-        for(int i=n-2; i>=0; i--){
-            if(ratings[i]>ratings[i+1]){
-                candies[i]=Math.max(candies[i],candies[i+1]+1);
-            }
+
+        for (int num : candies) {
+            sum += num;
         }
-        
-        int sum=0;
-        
-        
-        for(int num:candies) sum+=num;
-        
-        
+        // System.out.println(Arrays.toString(candies));
         return sum;
     }
 }
-// [2,34,1000,222,30,1,2,4,5,2,92]
+// [5,4,3,2,1]
+// [1,1,1,2,1]
