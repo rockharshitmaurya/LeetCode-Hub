854c3406d1f53de1428d3f6cf69910f7232ea568
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2023-09-20 20:03:01 +0530

    Time: 5 ms (63.89%), Space: 59.5 MB (47.50%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
@@ -1,29 +1,57 @@
 class Solution {
+
     public int minOperations(int[] nums, int x) {
-        long sum=0;
-        for(int num:nums) sum+=num;
-        if(sum<x) return -1;
-        sum-=x;
-        int i=0,j=0,temp=0,ans=-1;
-        while(i<nums.length && j<nums.length){
-            
-            if(temp<sum){
-                temp+=nums[j++];
-            }
-            if(temp>sum){
-                while(i<=j && temp>sum){
-                    temp-=nums[i];
-                    i++;
-                }
-            }
-            if(temp==sum){
-               ans=Math.max(ans,j-i+1);
-               temp-=nums[i];
-               i++; 
-            }
-            // System.out.println(temp+" "+sum);
+        int sum = 0;
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
         }
-        if(temp==sum)ans=Math.max(ans,j-i+1);
-        return ans==-1?-1:(nums.length-ans)+1;
+
+        if (sum < x) return -1; else if (sum == x) return n;
+
+        int i = n - 1, j = n - 1, ans = n + 1;
+
+        while (i >= 0 || j >= 0) {
+            while (i >= 0 && sum + nums[j] > x) {
+                sum -= nums[i--];
+               if(sum==x) ans = Math.min(ans, i + 1 + (n - (j + 1))); //j+1 because , J has not yet contributed to sum till now
+                
+            }
+            if (sum == x) {
+                // System.out.println("INSIDE " + i + " " + j + " " + sum);
+                ans = Math.min(ans, i + 1 + (n - (j + 1))); //j+1 because , J has not yet contributed to sum till now
+            }
+            // System.out.println(i + " " + j + " sum : " + sum);
+            sum += nums[j--];
+            if (sum == x) {
+                // System.out.println("INSIDE " + i + " " + j + " " + sum);
+                ans = Math.min(ans, i + 1 + (n - (j + 1)));
+            }
+        }
+
+        //         int ans = n + 1;
+
+        //         if (sum == x) ans = i;
+        //         i--;
+
+        return ans == n + 1 ? -1 : ans;
     }
 }
+
+
+// [1,1,4,2,3]
+// 5
+// [5,6,7,8,9]
+// 4
+// [3,2,20,1,1,3]
+// 10
+// [6016,5483,541,4325,8149,3515,7865,2209,9623,9763,4052,6540,2123,2074,765,7520,4941,5290,5868,6150,6006,6077,2856,7826,9119]
+// 31841
+// [5,2,3,1,1]
+// 5
+// [1,1]
+// 3
+// [5,1,4,2,3]
+// 6
+// [1,1,99]
+// 2
