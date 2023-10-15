e9a9dc7d0fae967a454360789f7ec3052587772a
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2023-10-15 22:25:40 +0530

    Time: 212 ms (10.36%), Space: 55 MB (20.12%) - LeetHub

---
+++ 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.java
@@ -0,0 +1,29 @@
+class Solution {
+HashMap<String,Long> map = new HashMap();
+    
+    public int numWays(int steps, int arrLen) {
+        
+        return (int) (ways(steps,arrLen,0) % ((Math.pow(10,9)) +7));
+    }
+    
+    public long ways(int steps,int arrLen,int index){
+        String curr = index + "->" + steps;
+        
+        if(index == 0 && steps == 0){
+            return 1;
+        }else if(index < 0 || (index >= arrLen) || steps == 0){
+            return 0;
+        }
+        
+        if(map.containsKey(curr)){
+            return map.get(curr);
+        }
+        long stay = ways(steps-1,arrLen,index);
+        long right = ways(steps-1,arrLen,index+1);
+        long left = ways(steps-1,arrLen,index-1);
+        
+        map.put(curr , (stay+right+left) % 1000000007);
+        
+        return (right + left + stay) % 1000000007;
+    }
+}
