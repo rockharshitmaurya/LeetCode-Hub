ff56267fcaff7209046a9f54f166abf8454275f1
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2024-09-25 22:26:20 +0530

    Time: 302 ms (64.52%), Space: 186.2 MB (92.26%) - LeetHub

--- 2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.java
+++ 2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.java
@@ -1,84 +1,66 @@
-class Solution {
-    class Node{
-    Node links[];
-    boolean flag;
-    int count;
-    public Node(){
-        links=new Node[26];
-        flag=false;
-        count=0;
-    }
-    boolean containsKey(char c){
-        return links[c-'a']!=null;
-    }
-    Node get(char c){
-        return links[c-'a'];
-    }
-    void put(char c,Node toadd){
-        links[c-'a']=toadd;
-    }
-    void setEnd(){
-        flag=true;
-    }
-    boolean isEnd(){
-        return flag;
-    }
-       
-}
-class Trie {
-    Node root;
-    public Trie() {
-        root=new Node();
-    }
-    
-    public void insert(String word) {
-        Node node=root;
-        for(char c:word.toCharArray()){
-            if(!node.containsKey(c)){
-                node.put(c,new Node());
-            }
-            node=node.get(c); 
-            node.count++;
-        }
-        node.setEnd();
-        
-    }
-    
-    public boolean search(String word) {
-        Node node=root;
-        for(char c:word.toCharArray()){
-            if(!node.containsKey(c)) return false;
-            
-            node=node.get(c);
-        }
-        return node.isEnd();
-    }
-    
-    public int startsWith(String prefix) {
+class Node {
+    int count = 0;
+    Node[] list = new Node[26];
 
-        Node node=root;
-        int ans=node.count;
-        for(char c:prefix.toCharArray()){
-            node=node.get(c);
-            ans+=node.count;
-        }
-        return ans;
+    public boolean containKey(char ch) {
+        return list[ch - 'a'] != null;
+    }
+
+    public Node get(char ch) {
+        return list[ch - 'a'];
+    }
+
+    public void put(char ch, Node new_node) {
+        list[ch - 'a'] = new_node;
+    }
+
+    public void inc(char ch) {
+        list[ch - 'a'].count++;
+    }
+
+    public int retCount(char ch) {
+        return list[ch - 'a'].count;
     }
 }
+
+class Solution {
+    private Node root;
+
+    public Solution() {
+        root = new Node();
+    }
+
+    public void insert(String word) {
+        Node node = root;
+        for (char ch : word.toCharArray()) {
+            if (!node.containKey(ch)) {
+                node.put(ch, new Node());
+            }
+            node.inc(ch);
+            node = node.get(ch);
+        }
+    }
+
+    public int search(String word) {
+        Node node = root;
+        int preCount = 0;
+        for (char ch : word.toCharArray()) {
+            preCount += node.retCount(ch);
+            node = node.get(ch);
+        }
+        return preCount;
+    }
+
     public int[] sumPrefixScores(String[] words) {
-        Trie root=new Trie();
-        int n=words.length;
-        int ans[]=new int[n];
-        
-        for(int i=0; i<n; i++){
-            root.insert(words[i]);
+        // This problem can be solved using the trie data structure
+        for (String word : words) {
+            insert(word);
         }
-        
-        for(int i=0; i<n; i++){
-            
-            ans[i]=root.startsWith(words[i]);
+        int n = words.length;
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++) {
+            res[i] = search(words[i]);
         }
-        
-        return ans;
+        return res;
     }
 }
