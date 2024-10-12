77a0b67bc6b94a1b7c25c3aef677acde5cff618f
Harshit Maurya <46915044+rockharshitmaurya@users.noreply.github.com>
2024-10-12 21:42:38 +0530

    Time: 154 ms (11.41%), Space: 85.5 MB (25.54%) - LeetHub

--- 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.java
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.java
@@ -1,23 +1,13 @@
 class Solution {
-    public int minGroups(int[][] intervals) {
-        int len=0;
-        for(int arr[]:intervals){
-            len = Math.max(len,arr[0]);
-            len = Math.max(len,arr[1]);
+public int minGroups(int[][] intervals) {
+        int res = 0, cur = 0, n = intervals.length, A[][] = new int[n * 2][2];
+        for (int i = 0; i < n; ++i) {
+            A[i * 2] = new int[]{intervals[i][0], 1};
+            A[i * 2 + 1] = new int[]{intervals[i][1] + 1, -1};
         }
-        int arr[]=new int[len+2];
-        
-        for(int sub[]:intervals){
-            arr[sub[0]]++; arr[sub[1]+1]--;
-        }
-        
-        for(int i=1; i<arr.length; i++){
-            arr[i]+=arr[i-1];
-        }
-        
-        int ans = 0 ;
-        
-        for(int num : arr) ans=Math.max(ans,num);
-        return ans;
+        Arrays.sort(A, Comparator.comparingInt(o -> o[0] * 3 + o[1]));
+        for (int[] a: A)
+            res = Math.max(res, cur += a[1]);
+        return res;
     }
 }
